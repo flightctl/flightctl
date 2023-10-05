94014fece3e34153b68baa62c70af48c3288b13e
Frank A. Zdarsky <fzdarsky@redhat.com>
2023-10-05 17:19:42 +0000

    Set up routes and insecure server

--- cmd/flightctl-server/main.go
+++ cmd/flightctl-server/main.go
@@ -1,10 +1,32 @@
 package main
 
 import (
+	"context"
 	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
+	oapimiddleware "github.com/deepmap/oapi-codegen/pkg/chi-middleware"
+	api "github.com/flightctl/flightctl/api/v1alpha1"
 	"github.com/flightctl/flightctl/internal/config"
+	"github.com/flightctl/flightctl/internal/service"
 	"github.com/flightctl/flightctl/internal/store"
+	"github.com/flightctl/flightctl/pkg/server"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+const (
+	gracefulShutdownTimeout     = 5 * time.Second
+	caCertValidityDays          = 365 * 10
+	serverCertValidityDays      = 365 * 1
+	clientBootStrapValidityDays = 365 * 1
+	signerCertName              = "csr-signer-ca"
+	serverCertName              = "server"
+	clientBootstrapCertName     = "client-bootstrap"
 )
 
 func main() {
@@ -25,5 +47,48 @@ func main() {
 	store := store.NewStore(db)
 	if err := store.InitialMigration(); err != nil {
 		log.Fatalf("running initial migration: %v", err)
+	}
+
+	swagger, err := api.GetSwagger()
+	if err != nil {
+		log.Fatalf("loading swagger spec: %v", err)
+	}
+	// Skip server name validation
+	swagger.Servers = nil
+
+	router := chi.NewRouter()
+	router.Use(
+		middleware.RequestID,
+		middleware.Logger,
+		middleware.Recoverer,
+		oapimiddleware.OapiRequestValidator(swagger),
+	)
+
+	h := service.NewServiceHandler(store)
+	server.HandlerFromMux(server.NewStrictHandler(h, nil), router)
+
+	srv := &http.Server{
+		Addr:         cfg.Service.Address,
+		Handler:      router,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	sigShutdown := make(chan os.Signal, 1)
+	signal.Notify(sigShutdown, os.Interrupt, syscall.SIGHUP, syscall.SIGTERM, syscall.SIGQUIT)
+	go func() {
+		<-sigShutdown
+		log.Println("Shutdown signal received")
+
+		ctxTimeout, cancel := context.WithTimeout(context.Background(), gracefulShutdownTimeout)
+		defer cancel()
+
+		srv.SetKeepAlivesEnabled(false)
+		srv.Shutdown(ctxTimeout)
+	}()
+
+	log.Printf("Listening on %s...", srv.Addr)
+	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		log.Fatal(err)
 	}
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/flightctl/flightctl
 go 1.19
 
 require (
+	github.com/deepmap/oapi-codegen v1.15.0
 	github.com/getkin/kin-openapi v0.120.0
 	github.com/go-chi/chi/v5 v5.0.10
 	github.com/google/uuid v1.3.1
@@ -17,6 +18,7 @@ require (
 	github.com/apapsch/go-jsonmerge/v2 v2.0.0 // indirect
 	github.com/go-openapi/jsonpointer v0.19.6 // indirect
 	github.com/go-openapi/swag v0.22.4 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/invopop/yaml v0.2.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
@@ -30,7 +32,7 @@ require (
 	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
 	github.com/perimeterx/marshmallow v1.1.5 // indirect
 	github.com/rogpeppe/go-internal v1.11.0 // indirect
-	golang.org/x/crypto v0.12.0 // indirect
-	golang.org/x/text v0.12.0 // indirect
+	golang.org/x/crypto v0.13.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
