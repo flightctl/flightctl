a0c6e7900c9d9e4a43ec063ada7cddd5c899ea5f
Sagi Dayan <sagidayan@gmail.com>
2024-02-12 14:59:34 +0200

    Wait for postgress in unit tests use podman
    Signed-off-by: Sagi Dayan <sagidayan@gmail.com>

--- hack/wait_for_postgres.sh
+++ hack/wait_for_postgres.sh
@@ -8,8 +8,10 @@ PG_HOST=127.0.0.1
 PG_PORT=5432
 export PGPASSWORD=adminpass
 
+set -x
+
 if [ -x "$(command -v pg_isready)" ]; then
-    until pg_isready -U ${PG_USER} --dbname ${PG_DATABASE} --host ${PG_HOST} --port ${PG_PORT}; do sleep 1; done
+    until podman exec flightctl-db pg_isready -U ${PG_USER} --dbname ${PG_DATABASE} --host ${PG_HOST} --port ${PG_PORT}; do sleep 1; done
 else
-    until psql -h ${PG_HOST} -U ${PG_USER} -d ${PG_DATABASE} -c "select 1" > /dev/null 2>&1; do sleep 1; done
+    until podman exec flightctl-db psql -h ${PG_HOST} -U ${PG_USER} -d ${PG_DATABASE} -c "select 1" > /dev/null 2>&1; do sleep 1; done
 fi
