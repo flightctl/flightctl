fa9d7f4e6762b661198a242d500f702bb41c3172
Miguel Angel Ajo Pelayo <majopela@redhat.com>
2024-02-16 16:26:43 +0100

    MGMT-16972: Deploy via kind+helm
    Enable deployment from kind+helm, replacing the podman deployment
    method.
    
    While we want to ensure that no hard K8s or OpenShift dependencies
    are introduced (and that was the main reason for podman), maintaing
    two sets of deployment files was error prone, and does't allow for
    testing the optional Secrets/ConfigFile dependencies eventually.
    
    Signed-off-by: Miguel Angel Ajo Pelayo <majopela@redhat.com>

---
+++ hack/install_kind.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+KIND_VERSION=0.22.0
+
+EXISTING_VERSION=$(kind version 2>&1 | awk '{print $2}' | sed 's/v//')
+
+if [ "$EXISTING_VERSION" == "$KIND_VERSION" ]; then
+     echo "Kind v${KIND_VERSION} already installed"
+else
+    echo "Installing kind v${KIND_VERSION}"
+    # Install kind
+    go install sigs.k8s.io/kind@v${KIND_VERSION}
+    sudo cp $(go env GOPATH)/bin/kind /usr/local/bin
+fi
+
+if which systemctl; then
+
+    if [ -f /etc/systemd/system/user@.service.d/delegate.conf ]; then
+        echo "Kind systemd rootless already configured" && exit 0
+    else
+        echo "Configuring Kind for rootless operation in Linux"
+        # Enable rootless Kind, see https://kind.sigs.k8s.io/docs/user/rootless/
+        sudo mkdir -p /etc/systemd/system/user@.service.d
+        cat << EOF | sudo tee /etc/systemd/system/user@.service.d/delegate.conf > /dev/null
+[Service]
+Delegate=yes
+EOF
+
+        sudo systemctl daemon-reload
+    fi
+fi
+
--- hack/wait_for_postgres.sh
+++ hack/wait_for_postgres.sh
@@ -2,16 +2,26 @@
 
 set -euo pipefail
 
+DB_NAMESPACE=flightctl-internal
 PG_USER=admin
 PG_DATABASE=flightctl
 PG_HOST=127.0.0.1
 PG_PORT=5432
 export PGPASSWORD=adminpass
 
-set -x
+METHOD=${1:-"kubectl"}
 
-if [ -x "$(command -v pg_isready)" ]; then
+if [ "$METHOD" == "podman" ];
+then
     until podman exec flightctl-db pg_isready -U ${PG_USER} --dbname ${PG_DATABASE} --host ${PG_HOST} --port ${PG_PORT}; do sleep 1; done
 else
-    until podman exec flightctl-db psql -h ${PG_HOST} -U ${PG_USER} -d ${PG_DATABASE} -c "select 1" > /dev/null 2>&1; do sleep 1; done
+    echo "Waiting for postgress deployment to be ready"
+    kubectl rollout status deployment flightctl-db -n ${DB_NAMESPACE} -w --timeout=300s
+
+    DB_POD=$(kubectl get pod -n ${DB_NAMESPACE} -L flightctl.service=flightctl-db --no-headers -o custom-columns=":metadata.name" --context kind-kind )
+    set -x
+    until kubectl exec --context kind-kind -n ${DB_NAMESPACE} ${DB_POD} -- psql -h ${PG_HOST} -U ${PG_USER} -d ${PG_DATABASE} -c "select 1" > /dev/null 2>&1;
+    do
+        sleep 1;
+    done
 fi
