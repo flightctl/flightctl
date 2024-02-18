8a0630d935e253d8a7f86e285a389d6e8a6688e6
Avishay Traeger <atraeger@redhat.com>
2024-02-18 16:49:34 +0200

    repotester to use generic conditions

--- internal/tasks/repotester/repotester.go
+++ internal/tasks/repotester/repotester.go
@@ -6,7 +6,6 @@ import (
 	api "github.com/flightctl/flightctl/api/v1alpha1"
 	"github.com/flightctl/flightctl/internal/store"
 	"github.com/flightctl/flightctl/internal/store/model"
-	"github.com/flightctl/flightctl/internal/util"
 	"github.com/flightctl/flightctl/pkg/log"
 	"github.com/flightctl/flightctl/pkg/reqid"
 	"github.com/go-chi/chi/v5/middleware"
@@ -14,7 +13,6 @@ import (
 	"github.com/go-git/go-git/v5/config"
 	"github.com/go-git/go-git/v5/plumbing/transport/http"
 	"github.com/go-git/go-git/v5/storage/memory"
-	"github.com/google/uuid"
 	"github.com/sirupsen/logrus"
 )
 
@@ -23,18 +21,20 @@ type API interface {
 }
 
 type RepoTester struct {
-	log       logrus.FieldLogger
-	repoStore store.Repository
+	log                    logrus.FieldLogger
+	repoStore              store.Repository
+	typeSpecificRepoTester TypeSpecificRepoTester
 }
 
 func NewRepoTester(log logrus.FieldLogger, store store.Store) *RepoTester {
 	return &RepoTester{
-		log:       log,
-		repoStore: store.Repository(),
+		log:                    log,
+		repoStore:              store.Repository(),
+		typeSpecificRepoTester: &GitRepoTester{},
 	}
 }
 
-func (r *RepoTester) TestRepo() {
+func (r *RepoTester) TestRepositories() {
 	reqid.OverridePrefix("repotester")
 	requestID := reqid.NextRequestID()
 	ctx := context.WithValue(context.Background(), middleware.RequestIDKey, requestID)
@@ -48,62 +48,53 @@ func (r *RepoTester) TestRepo() {
 		return
 	}
 
-	for _, repository := range repositories {
-		remote := git.NewRemote(memory.NewStorage(), &config.RemoteConfig{
-			Name:  repository.Name,
-			URLs:  []string{*repository.Spec.Data.Repo},
-			Fetch: []config.RefSpec{"HEAD"},
-		})
+	for i := range repositories {
+		repository := repositories[i]
+		accessErr := r.typeSpecificRepoTester.testAccess(&repository)
 
-		listOps := &git.ListOptions{}
-		if repository.Spec.Data.Username != nil && repository.Spec.Data.Password != nil {
-			listOps.Auth = &http.BasicAuth{
-				Username: *repository.Spec.Data.Username,
-				Password: *repository.Spec.Data.Password,
-			}
-		}
-
-		_, err = remote.List(listOps)
-
-		if repository.Status == nil {
-			repository.Status = model.MakeJSONField(api.RepositoryStatus{})
-		}
-		err = r.setAccessCondition(log, repository.Name, repository.OrgID, repository.Status.Data, err)
+		err := r.setAccessCondition(log, repository, accessErr)
 		if err != nil {
 			log.Errorf("Failed to update repository status for %s: %v", repository.Name, err)
 		}
 	}
 }
 
-func (r *RepoTester) setAccessCondition(log logrus.FieldLogger, name string, orgId uuid.UUID, repoStatus api.RepositoryStatus, err error) error {
-	timestamp := util.TimeStampStringPtr()
-	var lastTransitionTime *string
-	previousStatus := api.Unknown
-	if repoStatus.Conditions != nil && len(*repoStatus.Conditions) > 0 {
-		previousStatus = (*repoStatus.Conditions)[0].Status
-		lastTransitionTime = (*repoStatus.Conditions)[0].LastTransitionTime
-	}
-	condition := api.Condition{
-		Type:               api.Accessible,
-		LastHeartbeatTime:  timestamp,
-		LastTransitionTime: lastTransitionTime,
+type TypeSpecificRepoTester interface {
+	testAccess(repository *model.Repository) error
+}
+
+type GitRepoTester struct {
+}
+
+func (r *GitRepoTester) testAccess(repository *model.Repository) error {
+	remote := git.NewRemote(memory.NewStorage(), &config.RemoteConfig{
+		Name:  repository.Name,
+		URLs:  []string{*repository.Spec.Data.Repo},
+		Fetch: []config.RefSpec{"HEAD"},
+	})
+
+	listOps := &git.ListOptions{}
+	if repository.Spec.Data.Username != nil && repository.Spec.Data.Password != nil {
+		listOps.Auth = &http.BasicAuth{
+			Username: *repository.Spec.Data.Username,
+			Password: *repository.Spec.Data.Password,
+		}
 	}
 
-	if err == nil {
-		condition.Status = api.True
-		condition.Reason = util.StrToPtr("Accessible")
-		condition.Message = util.StrToPtr("Accessible")
-	} else {
-		condition.Status = api.False
-		condition.Reason = util.StrToPtr("Inaccessible")
-		condition.Message = util.StrToPtr(err.Error())
-	}
-	if previousStatus != condition.Status {
-		condition.LastTransitionTime = timestamp
-	}
+	_, err := remote.List(listOps)
+	return err
+}
 
-	repo := model.Repository{
-		Resource: model.Resource{OrgID: orgId, Name: name},
-		Status:   model.MakeJSONField(api.RepositoryStatus{Conditions: &[]api.Condition{condition}})}
-	return r.repoStore.UpdateStatusIgnoreOrg(&repo)
+func (r *RepoTester) setAccessCondition(log logrus.FieldLogger, repository model.Repository, err error) error {
+	if repository.Status == nil {
+		repository.Status = model.MakeJSONField(api.RepositoryStatus{Conditions: &[]api.Condition{}})
+	}
+	if repository.Status.Data.Conditions == nil {
+		repository.Status.Data.Conditions = &[]api.Condition{}
+	}
+	changed := api.SetStatusConditionByError(repository.Status.Data.Conditions, api.Accessible, "Accessible", "Inaccessible", err)
+	if changed {
+		return r.repoStore.UpdateStatusIgnoreOrg(&repository)
+	}
+	return nil
 }
