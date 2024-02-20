28240422558ecb2a6cb4cd69d9262a0224a229ae
Sam Batschelet <sbatsche@redhat.com>
2024-02-20 14:11:48 -0500

    internal/agent: add device status collector

---
+++ internal/agent/device/status/systemd.go
@@ -0,0 +1,77 @@
+package status
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/flightctl/flightctl/api/v1alpha1"
+	"github.com/flightctl/flightctl/pkg/executer"
+)
+
+const (
+	systemdCommand        = "/usr/bin/systemctl"
+	systemdCommandTimeout = 2 * time.Minute
+)
+
+var _ Exporter = (*SystemD)(nil)
+
+type SystemD struct {
+	exec          executer.Executer
+	mu            sync.Mutex
+	matchPatterns []string
+}
+
+func newSystemD(exec executer.Executer) *SystemD {
+	return &SystemD{
+		exec: exec,
+	}
+}
+
+type SystemDUnitList []SystemDUnitListEntry
+type SystemDUnitListEntry struct {
+	Unit        string `json:"unit"`
+	LoadState   string `json:"load"`
+	ActiveState string `json:"active"`
+	Sub         string `json:"sub"`
+	Description string `json:"description"`
+}
+
+func (c *SystemD) Export(ctx context.Context, status *v1alpha1.DeviceStatus) error {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	if c.matchPatterns == nil {
+		return nil
+	}
+
+	execCtx, cancel := context.WithTimeout(ctx, systemdCommandTimeout)
+	defer cancel()
+	args := append([]string{"list-units", "--all", "--output", "json"}, c.matchPatterns...)
+	out, errOut, exitCode := c.exec.ExecuteWithContext(execCtx, systemdCommand, args...)
+	if exitCode != 0 {
+		return fmt.Errorf("failed listing systemd units with code %d: %s", exitCode, errOut)
+	}
+
+	var units SystemDUnitList
+	if err := json.Unmarshal([]byte(out), &units); err != nil {
+		return fmt.Errorf("failed unmarshalling systemctl list-units output: %w", err)
+	}
+
+	deviceSystemdUnitStatus := make([]v1alpha1.DeviceSystemdUnitStatus, len(units))
+	for i, u := range units {
+		deviceSystemdUnitStatus[i].Name = u.Unit
+		deviceSystemdUnitStatus[i].LoadState = u.LoadState
+		deviceSystemdUnitStatus[i].ActiveState = u.ActiveState
+	}
+
+	status.SystemdUnits = &deviceSystemdUnitStatus
+	return nil
+}
+
+func (c *SystemD) SetMatchPatterns(matchPatterns []string) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.matchPatterns = matchPatterns
+}
---
+++ internal/agent/device/status/systeminfo.go
@@ -0,0 +1,61 @@
+package status
+
+import (
+	"context"
+	"fmt"
+	"runtime"
+
+	"github.com/flightctl/flightctl/api/v1alpha1"
+	"github.com/flightctl/flightctl/internal/tpm"
+	"github.com/google/cadvisor/fs"
+	"github.com/google/cadvisor/machine"
+	"github.com/google/cadvisor/utils/sysfs"
+)
+
+var _ Exporter = (*SystemInfo)(nil)
+
+type SystemInfo struct {
+	tpmChannel *tpm.TPM
+	systemInfo *v1alpha1.DeviceSystemInfo
+}
+
+func newSystemInfo(tpmChannel *tpm.TPM) *SystemInfo {
+	return &SystemInfo{tpmChannel: tpmChannel}
+}
+
+func (c *SystemInfo) Export(ctx context.Context, status *v1alpha1.DeviceStatus) error {
+	if c.systemInfo != nil {
+		status.SystemInfo = c.systemInfo
+		return nil
+	}
+
+	// collect what we can and use previous values on error
+	// retry until completed
+	systemInfo := &v1alpha1.DeviceSystemInfo{
+		Architecture:    runtime.GOARCH,
+		OperatingSystem: runtime.GOOS,
+	}
+
+	sysFs := sysfs.NewRealSysFs()
+	fsInfo, err := fs.NewFsInfo(fs.Context{})
+	if err != nil {
+		return fmt.Errorf("getting file system info: %w", err)
+	}
+	inHostNamespace := true
+	info, err := machine.Info(sysFs, fsInfo, inHostNamespace)
+	if err != nil {
+		return fmt.Errorf("getting machine info: %w", err)
+	}
+	systemInfo.BootID = info.BootID
+	systemInfo.MachineID = info.MachineID
+	systemInfo.Measurements = map[string]string{}
+	err = c.tpmChannel.GetPCRValues(systemInfo.Measurements)
+	if err != nil {
+		return fmt.Errorf("getting PCR values: %w", err)
+	}
+
+	c.systemInfo = systemInfo
+	status.SystemInfo = c.systemInfo
+
+	return nil
+}
