e5c226d8078585aac86275343aac65a9052e87d8
Miguel Angel Ajo Pelayo <majopela@redhat.com>
2024-03-05 17:42:04 +0100

    MGMT-17100: Factor out agent/server/db/client test harness
    This allows reusability of the test harness to provide an
    easy framework to write more mocked E2E tests.
    
    Signed-off-by: Miguel Angel Ajo Pelayo <majopela@redhat.com>

--- internal/agent/agent_test.go
+++ internal/agent/agent_test.go
@@ -1,87 +1,38 @@
-package agent
+package agent_test
 
 import (
-	"context"
 	"os"
 	"path/filepath"
 	"testing"
 	"time"
 
 	"github.com/flightctl/flightctl/api/v1alpha1"
-	"github.com/flightctl/flightctl/internal/config"
 	"github.com/flightctl/flightctl/internal/util"
-	"github.com/flightctl/flightctl/pkg/log"
-	testutil "github.com/flightctl/flightctl/test/util"
+	"github.com/flightctl/flightctl/test/harness"
 	"github.com/stretchr/testify/require"
 	"k8s.io/apimachinery/pkg/util/wait"
 	"sigs.k8s.io/yaml"
 )
 
 func TestDeviceAgent(t *testing.T) {
-	require := require.New(t)
 
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
+	require := require.New(t)
 
 	testDirPath := t.TempDir()
 	defer t.Cleanup(func() {
 		_ = os.RemoveAll(testDirPath)
 	})
-	err := makeTestDirs(testDirPath, []string{"/etc/issue.d/"})
-	require.NoError(err)
-
-	serverCfg := *config.NewDefault()
-	serverLog := log.InitLogs()
-
-	// create store
-	store, dbName := testutil.NewTestStore(t, serverCfg, serverLog)
-	serverCfg.Database.Name = dbName
-
-	// create certs
-	serverCfg.Service.CertStore = testDirPath
-	ca, serverCerts, clientCerts := testutil.NewTestCerts(t, &serverCfg)
-
-	// create server
-	server, listener := testutil.NewTestServer(t, serverLog, &serverCfg, store, ca, serverCerts)
-	serverCfg.Service.Address = listener.Addr().String()
-	defer listener.Close()
-
-	// start server
-	go func() {
-		err := server.Run()
+	h, err := harness.NewTestHarness(testDirPath, func(err error) {
+		// this inline function handles any errors that are returned from go routines
 		require.NoError(err)
-	}()
-
-	fetchSpecInterval := 1 * time.Second
-	statusUpdateInterval := 1 * time.Second
-
-	cfg := NewDefault()
-	cfg.CertDir = testDirPath
-	cfg.EnrollmentServerEndpoint = "https://" + serverCfg.Service.Address
-	cfg.EnrollmentUIEndpoint = "https://" + serverCfg.Service.Address
-	cfg.ManagementServerEndpoint = "https://" + serverCfg.Service.Address
-	cfg.FetchSpecInterval = util.Duration(fetchSpecInterval)
-	cfg.StatusUpdateInterval = util.Duration(statusUpdateInterval)
-	cfg.SetTestRootDir(testDirPath)
-
-	agentLog := log.InitLogs()
-
-	agentInstance := New(agentLog, cfg)
-
-	// start agent
-	go func() {
-		err := agentInstance.Run(ctx)
-		require.NoError(err)
-	}()
-
-	// create client to talk to the server
-	client, err := testutil.NewClient("https://"+listener.Addr().String(), ca.Config, clientCerts)
+	})
 	require.NoError(err)
+	defer h.Cleanup()
 
 	var deviceName string
 	// wait for the enrollment request to be created
 	err = wait.PollImmediate(100*time.Millisecond, 120*time.Second, func() (bool, error) {
-		listResp, err := client.ListEnrollmentRequestsWithResponse(ctx, &v1alpha1.ListEnrollmentRequestsParams{})
+		listResp, err := h.Client.ListEnrollmentRequestsWithResponse(h.Context, &v1alpha1.ListEnrollmentRequestsParams{})
 		if err != nil {
 			return false, err
 		}
@@ -102,12 +53,12 @@ func TestDeviceAgent(t *testing.T) {
 		Labels:   &map[string]string{"label": "value"},
 		Region:   util.StrToPtr("region"),
 	}
-	_, err = client.CreateEnrollmentRequestApprovalWithResponse(ctx, deviceName, approval)
+	_, err = h.Client.CreateEnrollmentRequestApprovalWithResponse(h.Context, deviceName, approval)
 	require.NoError(err)
 
 	// wait for the enrollment request to be approved
 	err = wait.PollImmediate(100*time.Millisecond, 120*time.Second, func() (bool, error) {
-		listResp, err := client.ListEnrollmentRequestsWithResponse(ctx, &v1alpha1.ListEnrollmentRequestsParams{})
+		listResp, err := h.Client.ListEnrollmentRequestsWithResponse(h.Context, &v1alpha1.ListEnrollmentRequestsParams{})
 		if err != nil {
 			return false, err
 		}
@@ -124,7 +75,7 @@ func TestDeviceAgent(t *testing.T) {
 	require.NoError(err)
 
 	// get the device
-	resp, err := client.ReadDeviceStatusWithResponse(ctx, deviceName)
+	resp, err := h.Client.ReadDeviceStatusWithResponse(h.Context, deviceName)
 	require.NoError(err)
 	require.Equal(200, resp.StatusCode())
 	device := *resp.JSON200
@@ -133,28 +84,18 @@ func TestDeviceAgent(t *testing.T) {
 	device.Spec, err = getTestSpec()
 	require.NoError(err)
 
-	_, err = client.ReplaceDeviceWithResponse(ctx, deviceName, device)
+	_, err = h.Client.ReplaceDeviceWithResponse(h.Context, deviceName, device)
 	require.NoError(err)
 
 	// wait for the device config to be written
 	err = wait.PollImmediate(100*time.Millisecond, 120*time.Second, func() (bool, error) {
-		_, err := os.Stat(filepath.Join(testDirPath, "/etc/motd"))
+		_, err := os.Stat(filepath.Join(h.TestDirPath, "/etc/motd"))
 		if err != nil && os.IsNotExist(err) {
 			return false, nil
 		}
 		return true, nil
 	})
 	require.NoError(err)
-}
-
-func makeTestDirs(tmpDirPath string, paths []string) error {
-	for _, path := range paths {
-		err := os.MkdirAll(filepath.Join(tmpDirPath, path), 0755)
-		if err != nil {
-			return err
-		}
-	}
-	return nil
 }
 
 func getTestSpec() (v1alpha1.DeviceSpec, error) {
