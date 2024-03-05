d6bb3aa6d5cfd70c36eb2b451bca75a6688ae871
Miguel Angel Ajo Pelayo <majopela@redhat.com>
2024-03-05 17:42:04 +0100

    MGMT-17100: Factor out agent/server/db/client test harness
    This allows reusability of the test harness to provide an
    easy framework to write more mocked E2E tests.
    
    Signed-off-by: Miguel Angel Ajo Pelayo <majopela@redhat.com>

--- test/util/util.go
+++ test/util/util.go
@@ -6,7 +6,6 @@ import (
 	"net/http"
 	"path/filepath"
 	"strings"
-	"testing"
 
 	"github.com/flightctl/flightctl/internal/api/client"
 	"github.com/flightctl/flightctl/internal/config"
@@ -15,7 +14,6 @@ import (
 	"github.com/flightctl/flightctl/internal/store"
 	"github.com/google/uuid"
 	"github.com/sirupsen/logrus"
-	"github.com/stretchr/testify/require"
 )
 
 const (
@@ -28,53 +26,59 @@ const (
 )
 
 // NewTestServer creates a new test server and returns the server and the listener listening on localhost's next available port.
-func NewTestServer(t testing.TB, log logrus.FieldLogger, cfg *config.Config, store store.Store, ca *crypto.CA, serverCerts *crypto.TLSCertificateConfig) (*server.Server, net.Listener) {
-	t.Helper()
-	require := require.New(t)
+func NewTestServer(log logrus.FieldLogger, cfg *config.Config, store store.Store, ca *crypto.CA, serverCerts *crypto.TLSCertificateConfig) (*server.Server, net.Listener, error) {
 	// create a listener using the next available port
 	tlsConfig, err := crypto.TLSConfigForServer(ca.Config, serverCerts)
-	require.NoError(err)
+	if err != nil {
+		return nil, nil, fmt.Errorf("NewTestServer: error creating TLS certs: %w", err)
+	}
 
 	// create a listener using the next available port
 	listener, err := server.NewTLSListener("", tlsConfig)
-	require.NoError(err)
+	if err != nil {
+		return nil, nil, fmt.Errorf("NewTLSListener: error creating TLS certs: %w", err)
+	}
 
-	return server.New(log, cfg, store, ca, listener), listener
+	return server.New(log, cfg, store, ca, listener), listener, nil
 }
 
 // NewTestStore creates a new test store and returns the store and the database name.
-func NewTestStore(t testing.TB, cfg config.Config, log *logrus.Logger) (store.Store, string) {
-	t.Helper()
-
-	require := require.New(t)
+func NewTestStore(cfg config.Config, log *logrus.Logger) (store.Store, string, error) {
 	// cfg.Database.Name = ""
 	dbTemp, err := store.InitDB(&cfg, log)
-	require.NoError(err)
+	if err != nil {
+		return nil, "", fmt.Errorf("NewTestStore: error initializing test DB: %w", err)
+	}
 	defer store.CloseDB(dbTemp)
 
 	randomDBName := fmt.Sprintf("_%s", strings.ReplaceAll(uuid.New().String(), "-", "_"))
-	t.Logf("DB name: %s", randomDBName)
+	log.Infof("DB name: %s", randomDBName)
 	dbTemp = dbTemp.Exec(fmt.Sprintf("CREATE DATABASE %s;", randomDBName))
-	require.NoError(dbTemp.Error)
+	if dbTemp.Error != nil {
+		return nil, "", fmt.Errorf("NewTestStore: creating test db %s: %w", randomDBName, dbTemp.Error)
+	}
 
 	cfg.Database.Name = randomDBName
 	db, err := store.InitDB(&cfg, log)
-	require.NoError(err)
+	if err != nil {
+		return nil, "", fmt.Errorf("NewTestStore: initializing test db %s: %w", randomDBName, err)
+	}
 
-	store := store.NewStore(db, log.WithField("pkg", "store"))
-	err = store.InitialMigration()
-	require.NoError(err)
+	dbStore := store.NewStore(db, log.WithField("pkg", "store"))
+	err = dbStore.InitialMigration()
+	if err != nil {
+		return nil, "", fmt.Errorf("NewTestStore: performing initial migration: %w", err)
+	}
 
-	return store, randomDBName
+	return dbStore, randomDBName, nil
 }
 
 // NewTestCerts creates new test certificates in the service certstore and returns the CA, server certificate, and client certificate.
-func NewTestCerts(t testing.TB, cfg *config.Config) (*crypto.CA, *crypto.TLSCertificateConfig, *crypto.TLSCertificateConfig) {
-	t.Helper()
-
-	require := require.New(t)
+func NewTestCerts(cfg *config.Config) (*crypto.CA, *crypto.TLSCertificateConfig, *crypto.TLSCertificateConfig, error) {
 	ca, _, err := crypto.EnsureCA(filepath.Join(cfg.Service.CertStore, "ca.crt"), filepath.Join(cfg.Service.CertStore, "ca.key"), "", "ca", caCertValidityDays)
-	require.NoError(err)
+	if err != nil {
+		return nil, nil, nil, fmt.Errorf("NewTestCerts: Ensuring CA: %w", err)
+	}
 
 	// default certificate hostnames to localhost if nothing else is configured
 	if len(cfg.Service.AltNames) == 0 {
@@ -82,12 +86,16 @@ func NewTestCerts(t testing.TB, cfg *config.Config) (*crypto.CA, *crypto.TLSCert
 	}
 
 	serverCerts, _, err := ca.EnsureServerCertificate(filepath.Join(cfg.Service.CertStore, "server.crt"), filepath.Join(cfg.Service.CertStore, "server.key"), cfg.Service.AltNames, serverCertValidityDays)
-	require.NoError(err)
+	if err != nil {
+		return nil, nil, nil, fmt.Errorf("NewTestCerts: Ensuring server certificate: %w", err)
+	}
 
 	clientCerts, _, err := ca.EnsureClientCertificate(filepath.Join(cfg.Service.CertStore, "client-enrollment.crt"), filepath.Join(cfg.Service.CertStore, "client-enrollment.key"), clientBootstrapCertName, clientBootStrapValidityDays)
-	require.NoError(err)
+	if err != nil {
+		return nil, nil, nil, fmt.Errorf("NewTestCerts: Ensuring client certificate: %w", err)
+	}
 
-	return ca, serverCerts, clientCerts
+	return ca, serverCerts, clientCerts, nil
 }
 
 // NewClient creates a new client with the given server URL and certificates.
