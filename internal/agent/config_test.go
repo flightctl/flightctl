0e7022695a499ff692acce8e5b374dbcde4b19cd
Sam Batschelet <sbatsche@redhat.com>
2024-03-13 13:22:28 -0400

    NO-ISSUE: internal/agent: add basic config test (#166)
    * NO-ISSUE: internal/agent: add basic config test
    ---------
    
    Signed-off-by: Sam Batschelet <sbatsche@redhat.com>

---
+++ internal/agent/config_test.go
@@ -0,0 +1,38 @@
+package agent
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+var yamlConfig = `management-endpoint: https://management.endpoint
+enrollment-endpoint: https://management.endpoint
+enrollment-ui-endpoint: https://ui.management.endpoint
+spec-fetch-interval: 0m10s
+status-update-interval: 0m10s`
+
+func TestParseConfigFile(t *testing.T) {
+	require := require.New(t)
+
+	tmpDir := t.TempDir()
+	filePath := tmpDir + "/config.yaml"
+	err := os.WriteFile(filePath, []byte(yamlConfig), 0600)
+	require.NoError(err)
+
+	cfg := NewDefault()
+	err = cfg.ParseConfigFile(filePath)
+	require.NoError(err)
+
+	// ensure override
+	require.Equal("https://management.endpoint", cfg.ManagementEndpoint)
+	require.Equal("https://management.endpoint", cfg.EnrollmentEndpoint)
+	require.Equal("https://ui.management.endpoint", cfg.EnrollmentUIEndpoint)
+	require.Equal("10s", cfg.SpecFetchInterval.String())
+	require.Equal("10s", cfg.StatusUpdateInterval.String())
+
+	// ensure defaults
+	require.Equal(DefaultConfigDir, cfg.ConfigDir)
+	require.Equal(DefaultDataDir, cfg.DataDir)
+}
