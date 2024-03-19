1b3f3628fe0f359c0afad0bf75b8154a462b0296
Miguel Angel Ajo Pelayo <majopela@redhat.com>
2024-03-19 12:54:53 +0100

    NO-ISSUE: Create an agent config we can use
    This exposes the service over the local address of our ethernet/wifi
    interface, by finding the exit route IP.
    
    Then it prepares this configuration along with the certificates into the
    bin/agent/ directory for later image embedding.
    
    Signed-off-by: Miguel Angel Ajo Pelayo <majopela@redhat.com>

---
+++ hack/prepare_agent_config.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+mkdir -p bin/agent/etc/flightctl/certs
+cp ~/.flightctl/certs/ca.crt bin/agent/etc/flightctl/certs/ca.crt
+cp ~/.flightctl/certs/client-enrollment.{crt,key} bin/agent/etc/flightctl/certs/
+
+IP=$(ip route get 1.1.1.1 | grep -oP 'src \K\S+')
+echo "Found ethernet interface IP: ${IP}"
+
+echo "Creating agent config"
+cat <<EOF > bin/agent/etc/flightctl/config.yaml
+management-endpoint: https://${IP}:3333
+enrollment-endpoint: https://${IP}:3333
+enrollment-ui-endpoint: https://${IP}:8080
+spec-fetch-interval: 0m10s
+status-update-interval: 0m10s
+EOF
