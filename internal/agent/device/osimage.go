179093c6b078ec65b1f856dbddc6fa05f275d3a7
Sam Batschelet <sbatsche@redhat.com>
2024-03-19 11:57:22 -0400

    MGMT-16600: os image controller (#183)
    - add os image controller
    ---------
    
    Signed-off-by: Sam Batschelet <sbatsche@redhat.com>

---
+++ internal/agent/device/osimage.go
@@ -0,0 +1,86 @@
+package device
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/flightctl/flightctl/api/v1alpha1"
+	"github.com/flightctl/flightctl/internal/agent/device/status"
+	"github.com/flightctl/flightctl/internal/container"
+	"github.com/flightctl/flightctl/pkg/executer"
+	"github.com/sirupsen/logrus"
+	"k8s.io/klog/v2"
+)
+
+const (
+	RebootingReason       = "Rebooting"
+	OsImageDegradedReason = "OSImageControllerDegraded"
+)
+
+type OSImageController struct {
+	bootc         *container.BootcCmd
+	statusManager status.Manager
+	log           *logrus.Logger
+	logPrefix     string
+}
+
+func NewOSImageController(
+	executor executer.Executer,
+	statusManager status.Manager,
+	log *logrus.Logger,
+	logPrefix string,
+) *OSImageController {
+	return &OSImageController{
+		bootc:         container.NewBootcCmd(executor),
+		statusManager: statusManager,
+		log:           log,
+		logPrefix:     logPrefix,
+	}
+}
+
+func (c *OSImageController) Sync(ctx context.Context, desired *v1alpha1.RenderedDeviceSpec) error {
+	klog.V(4).Infof("%s syncing device image", c.logPrefix)
+	defer klog.V(4).Infof("%s finished syncing device image", c.logPrefix)
+
+	err := c.ensureImage(ctx, desired)
+	if err != nil {
+		if err := c.statusManager.UpdateConditionError(ctx, OsImageDegradedReason, err); err != nil {
+			klog.Errorf("Failed to update condition: %v", err)
+		}
+		return err
+	}
+
+	return nil
+}
+
+func (c *OSImageController) ensureImage(ctx context.Context, desired *v1alpha1.RenderedDeviceSpec) error {
+	if desired.Os == nil {
+		klog.V(4).Infof("%s device os image is nil", c.logPrefix)
+		return nil
+	}
+
+	host, err := c.bootc.Status(ctx)
+	if err != nil {
+		return err
+	}
+
+	// TODO: handle the case where the host is reconciled but also in a dirty state (staged).
+	if container.IsOsImageReconciled(host, desired) {
+		klog.V(4).Infof("Host is reconciled to os image %s", desired.Os.Image)
+		return nil
+	}
+
+	image := desired.Os.Image
+	c.log.Infof("Switching to os image: %s", image)
+	if err := c.bootc.Switch(ctx, image); err != nil {
+		return err
+	}
+
+	// Update the status to progressing
+	if err := c.statusManager.UpdateCondition(ctx, v1alpha1.DeviceProgressing, v1alpha1.ConditionStatusTrue, RebootingReason, fmt.Sprintf("Rebooting into new os image: %s", image)); err != nil {
+		return err
+	}
+
+	c.log.Infof("Os image switch complete - rebooting into new image")
+	return c.bootc.Apply(ctx)
+}
---
+++ internal/agent/device/status/conditions.go
@@ -0,0 +1,59 @@
+package status
+
+import (
+	"github.com/flightctl/flightctl/api/v1alpha1"
+	"github.com/flightctl/flightctl/internal/util"
+)
+
+var (
+	DeviceConditionBootstrapReason string = "Bootstrap"
+	DeviceConditionExpectedReason  string = "AsExpected"
+)
+
+func DefaultConditions() *[]v1alpha1.Condition {
+	return &[]v1alpha1.Condition{
+		{
+			Type:   v1alpha1.DeviceProgressing,
+			Status: v1alpha1.ConditionStatusTrue,
+			Reason: &DeviceConditionBootstrapReason,
+		},
+		{
+			Type:   v1alpha1.DeviceAvailable,
+			Status: v1alpha1.ConditionStatusFalse,
+			Reason: &DeviceConditionBootstrapReason,
+		},
+		{
+			Type:   v1alpha1.DeviceDegraded,
+			Status: v1alpha1.ConditionStatusFalse,
+			Reason: &DeviceConditionExpectedReason,
+		},
+	}
+
+}
+
+// SetProgressingConditionByError sets the degraded condition based on the error.
+func SetDegradedConditionByError(conditions *[]v1alpha1.Condition, reason string, err error) bool {
+	condition := v1alpha1.FindStatusCondition(*conditions, v1alpha1.DeviceDegraded)
+	if err != nil {
+		condition.Status = v1alpha1.ConditionStatusTrue
+		condition.Reason = util.StrToPtr(reason)
+		condition.Message = util.StrToPtr(err.Error())
+	} else {
+		condition.Status = v1alpha1.ConditionStatusFalse
+		condition.Reason = &DeviceConditionExpectedReason
+		condition.Message = util.StrToPtr("All is well")
+	}
+
+	return v1alpha1.SetStatusCondition(conditions, *condition)
+}
+
+// SetProgressingCondition sets the progressing condition to true and adds the reason and message.
+func SetProgressingCondition(conditions *[]v1alpha1.Condition, conditionType v1alpha1.ConditionType, conditionStatus v1alpha1.ConditionStatus, reason string, message string) bool {
+	// TODO: ensure condition exists.
+	condition := v1alpha1.FindStatusCondition(*conditions, conditionType)
+	condition.Status = conditionStatus
+	condition.Reason = util.StrToPtr(reason)
+	condition.Message = util.StrToPtr(message)
+
+	return v1alpha1.SetStatusCondition(conditions, *condition)
+}
