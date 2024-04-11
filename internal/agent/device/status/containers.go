9d1eb79aaf40a6f01de8f956226fedbeda79e192
Ricardo Noriega <rnoriega@redhat.com>
2024-04-11 13:10:17 +0200

    NO-ISSUE: Adding support for CRIO container status
    Signed-off-by: Ricardo Noriega <rnoriega@redhat.com>

--- internal/agent/device/status/containers.go
+++ internal/agent/device/status/containers.go
@@ -16,11 +16,14 @@ const (
 	podmanCommand          = "/usr/bin/podman"
 	podmanCommandTimeout   = 2 * time.Minute
 	podmanContainerRunning = "running"
+	crioCommand            = "/usr/bin/crictl"
+	crioCommandTimeout     = 2 * time.Minute
+	crioContainerRunning   = "running"
 )
 
 var _ Exporter = (*Container)(nil)
 
-// Container collects podman container status.
+// Container collects container status.
 type Container struct {
 	exec          executer.Executer
 	mu            sync.Mutex
@@ -33,34 +36,47 @@ func newContainer(exec executer.Executer) *Container {
 	}
 }
 
-type PodmanList []PodmanListEntry
-type PodmanListEntry struct {
+type PodmanContainerList []PodmanContainerListEntry
+type PodmanContainerListEntry struct {
 	Names []string `json:"Names"`
 	State string   `json:"State"`
 	Image string   `json:"Image"`
 	Id    string   `json:"Id"`
 }
 
+type CrioContainerList struct {
+	Containers []CrioContainerListEntry `json:"containers"`
+}
+type CrioContainerListEntry struct {
+	Id       string `json:"id"`
+	Metadata struct {
+		Name string `json:"name"`
+	} `json:"metadata"`
+	Image string `json:"imageRef"`
+	State string `json:"state"`
+}
+
 type Shell interface {
 	Command(cmd string) (output []byte, err error)
 }
 
-func (c *Container) Export(ctx context.Context, status *v1alpha1.DeviceStatus) error {
-	execCtx, cancel := context.WithTimeout(ctx, podmanCommandTimeout)
+func (c *Container) PodmanExport(ctx context.Context, status *v1alpha1.DeviceStatus) error {
+	podmanExecCtx, cancel := context.WithTimeout(ctx, podmanCommandTimeout)
 	defer cancel()
 	args := []string{"ps", "-a", "--format", "json"}
 	for _, pattern := range c.matchPatterns {
 		args = append(args, "--filter")
 		args = append(args, fmt.Sprintf("name=%s", pattern))
 	}
-	out, errOut, exitCode := c.exec.ExecuteWithContext(execCtx, podmanCommand, args...)
-	if exitCode != 0 {
-		return fmt.Errorf("failed listing podman containers with code %d: %s", exitCode, errOut)
+	podmanOut, podmanErrOut, podmanExitCode := c.exec.ExecuteWithContext(podmanExecCtx, podmanCommand, args...)
+	if podmanExitCode != 0 {
+		return fmt.Errorf("failed listing podman containers with code %d: %s", podmanExitCode, podmanErrOut)
 	}
 
-	var containers PodmanList
-	if err := json.Unmarshal([]byte(out), &containers); err != nil {
-		return fmt.Errorf("failed unmarshalling podman list output: %w", err)
+	var containers PodmanContainerList
+	err := json.Unmarshal([]byte(podmanOut), &containers)
+	if err != nil {
+		return fmt.Errorf("failed unmarshalling podman containers: %s", err)
 	}
 
 	notRunning := 0
@@ -94,6 +110,79 @@ func (c *Container) Export(ctx context.Context, status *v1alpha1.DeviceStatus) e
 	}
 	v1alpha1.SetStatusCondition(status.Conditions, runningCondition)
 	status.Containers = &deviceContainerStatus
+
+	return nil
+}
+
+func (c *Container) CrioExport(ctx context.Context, status *v1alpha1.DeviceStatus) error {
+	crioExecCtx, cancel := context.WithTimeout(ctx, podmanCommandTimeout)
+	defer cancel()
+	args := []string{"ps", "-a", "--output", "json"}
+	for _, pattern := range c.matchPatterns {
+		args = append(args, "--name")
+		args = append(args, pattern)
+	}
+
+	crioOut, crioErrOut, crioExitCode := c.exec.ExecuteWithContext(crioExecCtx, crioCommand, args...)
+	if crioExitCode != 0 {
+		return fmt.Errorf("failed listing crio containers with code %d: %s", crioExitCode, crioErrOut)
+	}
+
+	var containers CrioContainerList
+	err := json.Unmarshal([]byte(crioOut), &containers)
+	if err != nil {
+		return fmt.Errorf("failed unmarshalling crio containers: %s", err)
+	}
+
+	notRunning := 0
+	runningCondition := v1alpha1.Condition{
+		Type: v1alpha1.DeviceContainersRunning,
+	}
+
+	deviceContainerStatus := make([]v1alpha1.ContainerStatus, len(containers.Containers))
+	for i, c := range containers.Containers {
+		deviceContainerStatus[i].Name = c.Metadata.Name
+		deviceContainerStatus[i].Status = c.State
+		deviceContainerStatus[i].Image = c.Image
+		deviceContainerStatus[i].Id = c.Id
+
+		if c.State != crioContainerRunning {
+			notRunning++
+		}
+	}
+
+	if notRunning == 0 {
+		runningCondition.Status = v1alpha1.ConditionStatusTrue
+		runningCondition.Reason = util.StrToPtr("Running")
+	} else {
+		runningCondition.Status = v1alpha1.ConditionStatusFalse
+		runningCondition.Reason = util.StrToPtr("NotRunning")
+		containerStr := "container"
+		if notRunning > 1 {
+			containerStr = "containers"
+		}
+		runningCondition.Message = util.StrToPtr(fmt.Sprintf("%d %s not running", notRunning, containerStr))
+	}
+
+	if status.Containers == nil {
+		status.Containers = &deviceContainerStatus
+	} else {
+		*status.Containers = append(*status.Containers, deviceContainerStatus...)
+	}
+
+	return nil
+}
+
+func (c *Container) Export(ctx context.Context, status *v1alpha1.DeviceStatus) error {
+	err := c.PodmanExport(ctx, status)
+	if err != nil {
+		return fmt.Errorf("failed exporting podman status: %w", err)
+	}
+
+	err = c.CrioExport(ctx, status)
+	if err != nil {
+		return fmt.Errorf("failed exporting crio status: %w", err)
+	}
 
 	return nil
 }
