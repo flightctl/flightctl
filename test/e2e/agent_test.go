8fb4b00b6dffa3f050a11362815e07439b5e70ef
Miguel Angel Ajo Pelayo <majopela@redhat.com>
2024-04-11 18:00:04 +0200

    NO-ISSUE: Add E2E harness with API and check TPM measurements
    This commits adds the basic E2E harness which includes the admin API
    client, moves the two e2e agent tests to the new harness and introduces
    a new test that:
    
    * Verifies the TPM reporting from within the VM
    
    This also switches the runner for E2E to the ginkgo binary which
    produces better reporting console output as well as better junit output.

--- test/e2e/agent_test.go
+++ test/e2e/agent_test.go
@@ -2,18 +2,18 @@ package agent_test
 
 import (
 	"fmt"
-	"os"
-	"path/filepath"
 	"strings"
 	"testing"
 
-	"github.com/flightctl/flightctl/test/harness/e2e/vm"
-	"github.com/google/uuid"
+	"github.com/flightctl/flightctl/internal/api/client"
+	"github.com/flightctl/flightctl/test/harness/e2e"
+	testutil "github.com/flightctl/flightctl/test/util"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"github.com/sirupsen/logrus"
 )
 
-const TIMEOUT = "30s"
+const TIMEOUT = "1m"
 const POLLING = "250ms"
 
 func TestAgent(t *testing.T) {
@@ -23,47 +23,103 @@ func TestAgent(t *testing.T) {
 
 var _ = Describe("VM Agent behavior", func() {
 	var (
-		testVM vm.TestVMInterface
+		harness *e2e.Harness
 	)
 
 	BeforeEach(func() {
-
-		currentWorkDirectory, err := os.Getwd()
-
-		Expect(err).ToNot(HaveOccurred())
-
-		testVM, err = vm.StartAndWaitForSSH(vm.TestVM{
-			TestDir:       GinkgoT().TempDir(),
-			VMName:        "flightctl-e2e-vm-" + uuid.New().String(),
-			DiskImagePath: filepath.Join(currentWorkDirectory, "../../bin/output/qcow2/disk.qcow2"),
-			VMUser:        "redhat",
-			SSHPassword:   "redhat",
-			SSHPort:       2233, // TODO: randomize and retry on erro
-		})
+		harness = e2e.NewTestHarness()
+		err := harness.VM.RunAndWaitForSSH()
+		if err != nil {
+			fmt.Println("============ Console output ============")
+			fmt.Println(harness.VM.GetConsoleOutput())
+			fmt.Println("========================================")
+		}
 		Expect(err).ToNot(HaveOccurred())
 	})
 
 	AfterEach(func() {
-		err := testVM.ForceDelete()
-		Expect(err).ToNot(HaveOccurred())
+		harness.Cleanup()
 	})
 
 	Context("vm", func() {
+
 		It("should print QR output to console", func() {
 			// Wait for the top-most part of the QR output to appear
-			Eventually(testVM.GetConsoleOutput, TIMEOUT, POLLING).Should(ContainSubstring("████████████████████████████████"))
-			output := testVM.GetConsoleOutput()
-			// this is only to show output when VERBOSE=true
+			Eventually(harness.VM.GetConsoleOutput, TIMEOUT, POLLING).Should(ContainSubstring("████████████████████████████████"))
+
 			fmt.Println("============ Console output ============")
-			lines := strings.Split(output, "\n")
+			lines := strings.Split(harness.VM.GetConsoleOutput(), "\n")
 			fmt.Println(strings.Join(lines[len(lines)-20:], "\n"))
 			fmt.Println("========================================")
 		})
+
 		It("should have flightctl-agent running", func() {
-			stdout, err := testVM.RunSSH([]string{"sudo", "systemctl", "status", "flightctl-agent"}, nil)
+			stdout, err := harness.VM.RunSSH([]string{"sudo", "systemctl", "status", "flightctl-agent"}, nil)
 			Expect(err).ToNot(HaveOccurred())
 			Expect(stdout.String()).To(ContainSubstring("Active: active (running)"))
+		})
+
+		It("should be reporting tpm info on enrollment request as well as device status", func() {
+			// Get the enrollment Request ID from the console output
+			enrollmentID := harness.GetEnrollmentIDFromConsole()
+			logrus.Infof("Enrollment ID found in VM console output: %s", enrollmentID)
+
+			// Wait for the device to create the enrollment request, and check the TPM details
+			enrollmentRequest := harness.WaitForEnrollmentRequest(enrollmentID)
+			Expect(enrollmentRequest.Spec).ToNot(BeNil())
+			Expect(enrollmentRequest.Spec.DeviceStatus).ToNot(BeNil())
+			Expect(enrollmentRequest.Spec.DeviceStatus.SystemInfo).ToNot(BeNil())
+			Expect(enrollmentRequest.Spec.DeviceStatus.SystemInfo.Measurements).ToNot(BeNil())
+			verifyPCRRegistersNotEmpty(enrollmentRequest.Spec.DeviceStatus.SystemInfo.Measurements)
+
+			// Approve the enrollment and wait for the device details to be populated by the agent
+			harness.ApproveEnrollment(enrollmentID, testutil.TestEnrollmentApproval())
+			logrus.Infof("Waiting for device %s to report status so we can check TPM PCRs again", enrollmentID)
+
+			// wait for the device to pickup enrollment and report measurements on device status
+			Eventually(getDeviceWithStatusSystemInfo, TIMEOUT, POLLING).WithArguments(
+				harness, enrollmentID).ShouldNot(BeNil())
+
+			device := getDeviceWithStatusSystemInfo(harness, enrollmentID)
+
+			// make sure that the PCR registers aren't empty
+			verifyPCRRegistersNotEmpty(device.JSON200.Status.SystemInfo.Measurements)
+
+			// verify that the measurements are the same as the ones we saw in the enrollment request
+			Expect(device.JSON200.Status.SystemInfo.Measurements).To(Equal(enrollmentRequest.Spec.DeviceStatus.SystemInfo.Measurements))
 		})
 	})
 
 })
+
+// PCR registers are initialized to a string of 00's at boot. Later on as
+// measurements are taken during boot steps, the PCR registers are updated inside the
+// TPM with: update_pcr(n, new_measurement){ pcr[n] = SHAx(pcr[n] || new_measurement) }
+// This means that the PCR registers should not be empty or all 0's after boot.
+// More details about the specific registers can be found here:
+// https://uapi-group.org/specifications/specs/linux_tpm_pcr_registry/
+func verifyPCRRegistersNotEmpty(measurements map[string]string) {
+	Expect(measurements).ToNot(BeNil())
+	for i := 1; i < 10; i++ {
+		pcrReg := fmt.Sprintf("pcr%02d", i)
+		pcr := measurements[pcrReg]
+		Expect(pcr).ToNot(BeNil())
+		// the length of the mesaurement will be different depending on the TPM
+		// sha algorithm so we just check that it's not empty or all 0's, we remove all
+		// 0's to make sure that the measurement isn't just a bunch of 0's
+		pcrRemove0x := strings.ReplaceAll(pcr, "0", "")
+		Expect(pcrRemove0x).ToNot(BeEmpty(), "PCR %s is empty or all 0's, is the VM booting in EFI secure boot?", pcrReg)
+	}
+}
+
+// get device from API, and return only devices that have a Status.SystemInfo
+func getDeviceWithStatusSystemInfo(harness *e2e.Harness, enrollmentID string) *client.ReadDeviceResponse {
+	device, err := harness.Client.ReadDeviceWithResponse(harness.Context, enrollmentID)
+	Expect(err).NotTo(HaveOccurred())
+	// we keep waiting for a 200 response, with filled in Status.SystemInfo
+	if device.JSON200 == nil || device.JSON200.Status == nil ||
+		device.JSON200.Status.SystemInfo == nil {
+		return nil
+	}
+	return device
+}
