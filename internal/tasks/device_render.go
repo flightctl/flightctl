2e218d9e585c91ed8b67f662d33e207222480513
Avishay Traeger <atraeger@redhat.com>
2024-06-18 13:35:31 +0300

    MGMT-18179: Remove duplication between config validate and render
    1. Combined the code that validates a fleet template and the code that
       renders a device since they are very similar.
    2. Add an error message to the condition if validation fails.

--- internal/tasks/device_render.go
+++ internal/tasks/device_render.go
@@ -15,6 +15,7 @@ import (
 	"github.com/flightctl/flightctl/pkg/log"
 	"github.com/flightctl/flightctl/pkg/reqid"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/google/uuid"
 	"github.com/sirupsen/logrus"
 	"sigs.k8s.io/yaml"
 )
@@ -45,13 +46,10 @@ func DeviceRender(taskManager TaskManager) {
 }
 
 type DeviceRenderLogic struct {
-	taskManager    TaskManager
-	log            logrus.FieldLogger
-	store          store.Store
-	resourceRef    ResourceReference
-	device         *api.Device
-	renderedConfig *config_latest_types.Config
-	repoNames      []string
+	taskManager TaskManager
+	log         logrus.FieldLogger
+	store       store.Store
+	resourceRef ResourceReference
 }
 
 func NewDeviceRenderLogic(taskManager TaskManager, log logrus.FieldLogger, store store.Store, resourceRef ResourceReference) DeviceRenderLogic {
@@ -63,56 +61,123 @@ func (t *DeviceRenderLogic) RenderDevice(ctx context.Context) error {
 
 	device, err := t.store.Device().Get(ctx, t.resourceRef.OrgID, t.resourceRef.Name)
 	if err != nil {
-		return err
-	}
-	t.device = device
-	emptyIgnitionConfig, _, _ := config_latest.ParseCompatibleVersion([]byte("{\"ignition\": {\"version\": \"3.4.0\"}"))
-	t.renderedConfig = &emptyIgnitionConfig
-
-	invalidConfigs := []string{}
-	if device.Spec != nil && device.Spec.Config != nil {
-		for i := range *device.Spec.Config {
-			configItem := (*device.Spec.Config)[i]
-			name, err := t.handleConfigItem(ctx, &configItem)
-			if err != nil {
-				if errors.Is(err, ErrUnknownConfigName) {
-					name = "<unknown>"
-				}
-				invalidConfigs = append(invalidConfigs, name)
-			}
-			t.log.Errorf("failed rendering config %s for device %s/%s: %v", name, t.resourceRef.OrgID, t.resourceRef.Name, err)
-		}
+		return fmt.Errorf("failed getting device %s/%s: %w", t.resourceRef.OrgID, t.resourceRef.Name, err)
 	}
 
+	// If device.Spec or device.Spec.Config are nil, we still want to render an empty ignition config
+	var config *[]api.DeviceSpec_Config_Item
+	if device.Spec != nil {
+		config = device.Spec.Config
+	}
+
+	renderedConfig, repoNames, renderErr := renderConfig(ctx, t.resourceRef.OrgID, t.store, config, false)
+
+	// Set the many-to-many relationship with the repos (we do this even if the render failed so that we will
+	// render the device again if the repository is updated, and then it might be fixed).
+	// This only applies to devices that don't belong to a fleet, because otherwise the fleet will be
+	// notified about changes to the repository.
 	if device.Metadata.Owner == nil || *device.Metadata.Owner == "" {
-		// Set the many-to-may relationship with the repos (we do this even if the validation failed so that we will
-		// validate the fleet again if the repository is updated, and then it might be fixed)
-		err = t.store.Device().OverwriteRepositoryRefs(ctx, t.resourceRef.OrgID, *device.Metadata.Name, t.repoNames...)
+		err = t.store.Device().OverwriteRepositoryRefs(ctx, t.resourceRef.OrgID, *device.Metadata.Name, repoNames...)
 		if err != nil {
 			return t.setStatus(ctx, fmt.Errorf("setting repository references: %w", err))
 		}
 	}
 
-	if len(invalidConfigs) != 0 {
-		configurationStr := "configuration"
-		if len(invalidConfigs) > 1 {
-			configurationStr += "s"
-		}
-		err = fmt.Errorf("device has %d invalid %s: %s", len(invalidConfigs), configurationStr, strings.Join(invalidConfigs, ", "))
-		return t.setStatus(ctx, err)
+	if renderErr != nil {
+		return t.setStatus(ctx, renderErr)
 	}
 
-	configjson, err := json.Marshal(t.renderedConfig)
-	if err != nil {
-		return t.setStatus(ctx, err)
-	}
-
-	err = t.store.Device().UpdateRendered(ctx, t.resourceRef.OrgID, t.resourceRef.Name, string(configjson))
+	err = t.store.Device().UpdateRendered(ctx, t.resourceRef.OrgID, t.resourceRef.Name, string(renderedConfig))
 	return t.setStatus(ctx, err)
 }
 
-func (t *DeviceRenderLogic) handleConfigItem(ctx context.Context, configItem *api.DeviceSpec_Config_Item) (string, error) {
+func (t *DeviceRenderLogic) setStatus(ctx context.Context, renderErr error) error {
+	condition := api.Condition{Type: api.DeviceSpecValid}
 
+	if renderErr == nil {
+		condition.Status = api.ConditionStatusTrue
+		condition.Reason = util.StrToPtr("Valid")
+	} else {
+		condition.Status = api.ConditionStatusFalse
+		condition.Reason = util.StrToPtr("Invalid")
+		condition.Message = util.StrToPtr(renderErr.Error())
+	}
+
+	err := t.store.Device().SetServiceConditions(ctx, t.resourceRef.OrgID, t.resourceRef.Name, []api.Condition{condition})
+	if err != nil {
+		t.log.Errorf("Failed setting condition for device %s/%s: %v", t.resourceRef.OrgID, t.resourceRef.Name, err)
+	}
+	return renderErr
+}
+
+type renderConfigArgs struct {
+	orgId          uuid.UUID
+	store          store.Store
+	ignitionConfig *config_latest_types.Config
+	repoNames      []string
+	validateOnly   bool
+}
+
+func renderConfig(ctx context.Context, orgId uuid.UUID, store store.Store, config *[]api.DeviceSpec_Config_Item, validateOnly bool) (renderedConfig []byte, repoNames []string, err error) {
+	args := renderConfigArgs{}
+	emptyIgnitionConfig, _, _ := config_latest.ParseCompatibleVersion([]byte("{\"ignition\": {\"version\": \"3.4.0\"}"))
+	args.ignitionConfig = &emptyIgnitionConfig
+	args.validateOnly = validateOnly
+	args.orgId = orgId
+	args.store = store
+
+	err = renderConfigItems(ctx, config, &args)
+	if err != nil {
+		return nil, args.repoNames, err
+	}
+
+	if validateOnly {
+		return nil, args.repoNames, nil
+	}
+
+	renderedConfig, err = json.Marshal(args.ignitionConfig)
+	if err != nil {
+		return nil, args.repoNames, fmt.Errorf("failed marshalling configuration: %w", err)
+	}
+
+	return renderedConfig, args.repoNames, nil
+}
+
+func renderConfigItems(ctx context.Context, config *[]api.DeviceSpec_Config_Item, args *renderConfigArgs) error {
+	if config == nil {
+		return nil
+	}
+
+	invalidConfigs := []string{}
+	var firstError error
+	for i := range *config {
+		configItem := (*config)[i]
+		name, err := renderConfigItem(ctx, &configItem, args)
+		if err != nil {
+			if errors.Is(err, ErrUnknownConfigName) {
+				name = "<unknown>"
+			}
+			invalidConfigs = append(invalidConfigs, name)
+			if len(invalidConfigs) == 1 {
+				firstError = err
+			}
+		}
+	}
+
+	if len(invalidConfigs) != 0 {
+		configurationStr := "configuration"
+		errorStr := "Error"
+		if len(invalidConfigs) > 1 {
+			configurationStr += "s"
+			errorStr = "First error"
+		}
+		return fmt.Errorf("%d invalid %s: %s. %s: %v", len(invalidConfigs), configurationStr, strings.Join(invalidConfigs, ", "), errorStr, firstError)
+	}
+
+	return nil
+}
+
+func renderConfigItem(ctx context.Context, configItem *api.DeviceSpec_Config_Item, args *renderConfigArgs) (string, error) {
 	disc, err := configItem.Discriminator()
 	if err != nil {
 		return "", fmt.Errorf("%w: failed getting discriminator: %w", ErrUnknownConfigName, err)
@@ -120,36 +185,40 @@ func (t *DeviceRenderLogic) handleConfigItem(ctx context.Context, configItem *ap
 
 	switch disc {
 	case string(api.TemplateDiscriminatorGitConfig):
-		return t.handleGitConfig(ctx, configItem)
+		return renderGitConfig(ctx, configItem, args)
 	case string(api.TemplateDiscriminatorKubernetesSec):
-		return t.handleK8sConfig(configItem)
+		return renderK8sConfig(configItem)
 	case string(api.TemplateDiscriminatorInlineConfig):
-		return t.handleInlineConfig(configItem)
+		return renderInlineConfig(configItem, args)
 	default:
 		return "", fmt.Errorf("%w: unsupported discriminator: %s", ErrUnknownConfigName, disc)
 	}
 }
 
-func (t *DeviceRenderLogic) handleGitConfig(ctx context.Context, configItem *api.DeviceSpec_Config_Item) (string, error) {
+func renderGitConfig(ctx context.Context, configItem *api.DeviceSpec_Config_Item, args *renderConfigArgs) (string, error) {
 	gitSpec, err := configItem.AsGitConfigProviderSpec()
 	if err != nil {
 		return "", fmt.Errorf("%w: failed getting config item as GitConfigProviderSpec: %w", ErrUnknownConfigName, err)
 	}
 
-	t.repoNames = append(t.repoNames, gitSpec.GitRef.Repository)
-	repo, err := t.store.Repository().GetInternal(ctx, t.resourceRef.OrgID, gitSpec.GitRef.Repository)
+	args.repoNames = append(args.repoNames, gitSpec.GitRef.Repository)
+	repo, err := args.store.Repository().GetInternal(ctx, args.orgId, gitSpec.GitRef.Repository)
 	if err != nil {
-		return gitSpec.Name, fmt.Errorf("failed fetching specified Repository definition %s/%s: %w", t.resourceRef.OrgID, gitSpec.GitRef.Repository, err)
+		return gitSpec.Name, fmt.Errorf("failed fetching specified Repository definition %s/%s: %w", args.orgId, gitSpec.GitRef.Repository, err)
 	}
 
 	if repo.Spec == nil {
-		return gitSpec.Name, fmt.Errorf("failed fetching specified Repository definition %s/%s: %w", t.resourceRef.OrgID, gitSpec.GitRef.Repository, err)
+		return gitSpec.Name, fmt.Errorf("empty Repository definition %s/%s: %w", args.orgId, gitSpec.GitRef.Repository, err)
+	}
+
+	if args.validateOnly {
+		return gitSpec.Name, nil
 	}
 
 	// TODO: Use local cache
 	mfs, _, err := CloneGitRepo(repo, &gitSpec.GitRef.TargetRevision, nil)
 	if err != nil {
-		return gitSpec.Name, fmt.Errorf("failed cloning specified git repository %s/%s: %w", t.resourceRef.OrgID, gitSpec.GitRef.Repository, err)
+		return gitSpec.Name, fmt.Errorf("failed cloning specified git repository %s/%s: %w", args.orgId, gitSpec.GitRef.Repository, err)
 	}
 
 	// Create an ignition from the git subtree and merge it into the rendered config
@@ -157,33 +226,26 @@ func (t *DeviceRenderLogic) handleGitConfig(ctx context.Context, configItem *api
 	if err != nil {
 		return gitSpec.Name, fmt.Errorf("failed parsing git config item %s: %w", gitSpec.Name, err)
 	}
-	renderedConfig := config_latest.Merge(*t.renderedConfig, *ignitionConfig)
-	t.renderedConfig = &renderedConfig
+	mergedConfig := config_latest.Merge(*args.ignitionConfig, *ignitionConfig)
+	args.ignitionConfig = &mergedConfig
 
 	return gitSpec.Name, nil
 }
 
 // TODO: implement
-func (t *DeviceRenderLogic) handleK8sConfig(configItem *api.DeviceSpec_Config_Item) (string, error) {
+func renderK8sConfig(configItem *api.DeviceSpec_Config_Item) (string, error) {
 	k8sSpec, err := configItem.AsKubernetesSecretProviderSpec()
 	if err != nil {
 		return "", fmt.Errorf("%w: failed getting config item as KubernetesSecretProviderSpec: %w", ErrUnknownConfigName, err)
 	}
 
-	return k8sSpec.Name, fmt.Errorf("service does not yet support kubernetes config")
+	return k8sSpec.Name, fmt.Errorf("kubernetes config type not yet supported")
 }
 
-func (t *DeviceRenderLogic) handleInlineConfig(configItem *api.DeviceSpec_Config_Item) (string, error) {
+func renderInlineConfig(configItem *api.DeviceSpec_Config_Item, args *renderConfigArgs) (string, error) {
 	inlineSpec, err := configItem.AsInlineConfigProviderSpec()
 	if err != nil {
 		return "", fmt.Errorf("%w: failed getting config item as InlineConfigProviderSpec: %w", ErrUnknownConfigName, err)
-	}
-
-	// Add this inline config into the unrendered config
-	newConfig := &api.TemplateVersionStatus_Config_Item{}
-	err = newConfig.FromInlineConfigProviderSpec(inlineSpec)
-	if err != nil {
-		return inlineSpec.Name, fmt.Errorf("failed creating inline config from item %s: %w", inlineSpec.Name, err)
 	}
 
 	// Convert yaml to json
@@ -202,26 +264,9 @@ func (t *DeviceRenderLogic) handleInlineConfig(configItem *api.DeviceSpec_Config
 		return inlineSpec.Name, fmt.Errorf("failed parsing inline config item %s: %w", inlineSpec.Name, err)
 	}
 
-	renderedConfig := config_latest.Merge(*t.renderedConfig, ignitionConfig)
-	t.renderedConfig = &renderedConfig
+	if !args.validateOnly {
+		mergedConfig := config_latest.Merge(*args.ignitionConfig, ignitionConfig)
+		args.ignitionConfig = &mergedConfig
+	}
 	return inlineSpec.Name, nil
-}
-
-func (t *DeviceRenderLogic) setStatus(ctx context.Context, renderErr error) error {
-	condition := api.Condition{Type: api.DeviceSpecValid}
-
-	if renderErr == nil {
-		condition.Status = api.ConditionStatusTrue
-		condition.Reason = util.StrToPtr("Valid")
-	} else {
-		condition.Status = api.ConditionStatusFalse
-		condition.Reason = util.StrToPtr("Invalid")
-		condition.Message = util.StrToPtr(fmt.Sprintf("Device configuration is invalid: %v", renderErr))
-	}
-
-	err := t.store.Device().SetServiceConditions(ctx, t.resourceRef.OrgID, t.resourceRef.Name, []api.Condition{condition})
-	if err != nil {
-		t.log.Errorf("Failed setting condition for device %s/%s: %v", t.resourceRef.OrgID, t.resourceRef.Name, err)
-	}
-	return renderErr
 }
