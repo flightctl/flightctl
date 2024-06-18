1b13190acff6c9d06aca3b6b5ba8442b8a0597e6
Miguel Angel Ajo Pelayo <majopela@redhat.com>
2024-06-18 12:37:29 +0200

    OCTOET-476: Create agent api/client definitions
    The openapi.yaml for the agent references the main one for schemas.
    
    Subsequent commits will remove some of the methods and paths from
    the general server, and move those into the agent only api.
    
    OCTOET-476: update to oapi-gen v2.3.0
    
    We reference the existing methods in the main openapi.yaml,
    but this is broken in oapi-gen v1.xx, v2.3.0 improves
    generation a bit but some details are still broken
    (we apply workaround).

---
+++ internal/api/server/agent/docs.go
@@ -0,0 +1,3 @@
+package server
+
+//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen@v2.3.0 --config=server.gen.cfg ../../../../api/v1alpha1/agent/openapi.yaml
---
+++ internal/api/server/agent/server.go
@@ -0,0 +1,6 @@
+package server
+
+// Service is a wrapper around the generated server interface.
+type Service interface {
+	StrictServerInterface
+}
