48c63fe546a17e39b3e93814cc51f845d5f0a2b7
Ricardo Noriega <rnoriega@redhat.com>
2024-07-04 19:55:06 +0200

    EDM-192: Render HttpProviderConfig
    Signed-off-by: Ricardo Noriega <rnoriega@redhat.com>

--- internal/tasks/device_render.go
+++ internal/tasks/device_render.go
@@ -2,9 +2,14 @@ package tasks
 
 import (
 	"context"
+	"crypto/tls"
+	"crypto/x509"
+	"encoding/base64"
 	"encoding/json"
 	"errors"
 	"fmt"
+	"io"
+	"net/http"
 	"path/filepath"
 	"strings"
 
@@ -187,6 +192,8 @@ func renderConfigItem(ctx context.Context, configItem *api.DeviceSpec_Config_Ite
 		return renderK8sConfig(configItem, args)
 	case string(api.TemplateDiscriminatorInlineConfig):
 		return renderInlineConfig(configItem, args)
+	case string(api.TemplateDiscriminatorHttpConfig):
+		return renderHttpProviderConfig(ctx, configItem, args)
 	default:
 		return "", fmt.Errorf("%w: unsupported discriminator: %s", ErrUnknownConfigName, disc)
 	}
@@ -292,4 +299,113 @@ func renderInlineConfig(configItem *api.DeviceSpec_Config_Item, args *renderConf
 		args.ignitionConfig = &mergedConfig
 	}
 	return inlineSpec.Name, nil
+}
+
+func renderHttpProviderConfig(ctx context.Context, configItem *api.DeviceSpec_Config_Item, args *renderConfigArgs) (string, error) {
+	httpConfigProviderSpec, err := configItem.AsHttpConfigProviderSpec()
+	if err != nil {
+		return "", fmt.Errorf("%w: failed getting config item as HttpConfigProviderSpec: %w", ErrUnknownConfigName, err)
+	}
+	args.repoNames = append(args.repoNames, httpConfigProviderSpec.HttpRef.Repository)
+	repo, err := args.store.Repository().GetInternal(ctx, args.orgId, httpConfigProviderSpec.HttpRef.Repository)
+	if err != nil {
+		return httpConfigProviderSpec.Name, fmt.Errorf("failed fetching specified Repository definition %s/%s: %w", args.orgId, httpConfigProviderSpec.HttpRef.Repository, err)
+	}
+	if repo.Spec == nil {
+		return httpConfigProviderSpec.Name, fmt.Errorf("empty Repository definition %s/%s: %w", args.orgId, httpConfigProviderSpec.HttpRef.Repository, err)
+	}
+	repoURL, err := repo.Spec.Data.GetRepoURL()
+	if err != nil {
+		return "", err
+	}
+	repoURL = repoURL + *httpConfigProviderSpec.HttpRef.Suffix
+	if args.validateOnly {
+		return httpConfigProviderSpec.Name, nil
+	}
+	req, err := http.NewRequest("GET", repoURL, nil)
+	if err != nil {
+		return "", fmt.Errorf("creating request: %w", err)
+	}
+
+	repoHttpSpec, err := repo.Spec.Data.GetHttpRepoSpec()
+	if err != nil {
+		return "", err
+	}
+	if repoHttpSpec.HttpConfig.Username != nil && repoHttpSpec.HttpConfig.Password != nil {
+		req.SetBasicAuth(*repoHttpSpec.HttpConfig.Username, *repoHttpSpec.HttpConfig.Password)
+	}
+	if repoHttpSpec.HttpConfig.Token != nil {
+		req.Header.Set("Authorization", "Bearer "+*repoHttpSpec.HttpConfig.Token)
+	}
+
+	tlsConfig := &tls.Config{}
+	if repoHttpSpec.HttpConfig.TlsCrt != nil && repoHttpSpec.HttpConfig.TlsKey != nil {
+		cert, err := base64.StdEncoding.DecodeString(*repoHttpSpec.HttpConfig.TlsCrt)
+		if err != nil {
+			return "", err
+		}
+
+		key, err := base64.StdEncoding.DecodeString(*repoHttpSpec.HttpConfig.TlsKey)
+		if err != nil {
+			return "", err
+		}
+
+		tlsPair, err := tls.X509KeyPair(cert, key)
+		if err != nil {
+			return "", err
+		}
+
+		tlsConfig.Certificates = []tls.Certificate{tlsPair}
+	}
+
+	if repoHttpSpec.HttpConfig.CaCrt != nil {
+		ca, err := base64.StdEncoding.DecodeString(*repoHttpSpec.HttpConfig.CaCrt)
+		if err != nil {
+			return "", err
+		}
+
+		rootCAs, _ := x509.SystemCertPool()
+		if rootCAs == nil {
+			rootCAs = x509.NewCertPool()
+		}
+		rootCAs.AppendCertsFromPEM(ca)
+		tlsConfig.RootCAs = rootCAs
+	}
+	if repoHttpSpec.HttpConfig.SkipServerVerification != nil {
+		tlsConfig.InsecureSkipVerify = *repoHttpSpec.HttpConfig.SkipServerVerification //nolint:gosec
+
+	}
+
+	// Set up the HTTP client with the configured TLS settings
+	client := &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: tlsConfig,
+		},
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", fmt.Errorf("sending request: %w", err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("unexpected status code %d", resp.StatusCode)
+	}
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", fmt.Errorf("reading response body: %w", err)
+	}
+
+	// Convert body to ignition config
+	ignitionWrapper, err := ignition.NewWrapper()
+	if err != nil {
+		return httpConfigProviderSpec.Name, fmt.Errorf("failed to create ignition wrapper: %w", err)
+	}
+
+	ignitionWrapper.SetFile(httpConfigProviderSpec.HttpRef.FilePath, body, 0o644)
+	if !args.validateOnly {
+		args.ignitionConfig = lo.ToPtr(ignitionWrapper.Merge(*args.ignitionConfig))
+	}
+
+	return httpConfigProviderSpec.Name, nil
 }
