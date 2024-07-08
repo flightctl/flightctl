6e7d99e91ec0982acaeba1c1826f5402b4cdde39
Sam Batschelet <sbatsche@redhat.com>
2024-07-08 11:28:06 -0400

    NO_ISSUE: simulator enroll
    Signed-off-by: Sam Batschelet <sbatsche@redhat.com>

--- test/scripts/prepare_agent_config.sh
+++ test/scripts/prepare_agent_config.sh
@@ -4,6 +4,7 @@ SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 IP=$("${SCRIPT_DIR}"/get_ext_ip.sh)
 OUTFILE=bin/agent/etc/flightctl/config.yaml
 mkdir -p bin/agent/etc/flightctl/certs
+mkdir -p bin/agent/etc/flightctl/data
 cp ~/.flightctl/certs/ca.crt bin/agent/etc/flightctl/certs/ca.crt
 cp ~/.flightctl/certs/client-enrollment.{crt,key} bin/agent/etc/flightctl/certs/
 
@@ -17,6 +18,13 @@ enrollment-service:
     certificate-authority: certs/ca.crt
     server: https://${IP}:7443
   enrollment-ui-endpoint: https://${IP}:8080
+management-service:
+  service:
+    server: https://${IP}:7443
+    certificate-authority: certs/ca.crt
+  authentication:
+    client-certificate: certs/client-enrollment.crt
+    client-key: certs/client-enrollment.key
 spec-fetch-interval: 0m10s
 status-update-interval: 0m10s
 tpm-path: /dev/tpmrm0
