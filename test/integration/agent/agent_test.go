07b1d7e7f40d818bf6ce340375e28e87a5045511
Ori Amizur <oamizur@redhat.com>
2024-07-21 18:33:13 +0300

    EDN-221: Secret data should not be decoded
    When handling kubernetes secrets, they are currently expected to be base64 encoded.
    But the data itself as received in kubernetes API is not encoded since
    JSON decodes it before the data can be handled by the caller.
    So the current base64 decoding should be removed.

--- test/integration/agent/agent_test.go
+++ test/integration/agent/agent_test.go
@@ -1,7 +1,6 @@
 package agent
 
 import (
-	"encoding/base64"
 	"fmt"
 	"io/fs"
 	"net/http"
@@ -254,7 +253,7 @@ func mockSecret(mockK8sClient *k8sclient.MockK8SClient, secrets map[string]strin
 				Namespace: "secret-namespace",
 			},
 			Data: lo.MapValues(secrets, func(v, _ string) []byte {
-				return []byte(base64.StdEncoding.EncodeToString([]byte(v)))
+				return []byte(v)
 			}),
 		}, nil).AnyTimes()
 }
