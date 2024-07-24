1271c9b33da74cf3e5534b3ef1ebe7a73fc0b556
Ricardo Noriega <rnoriega@redhat.com>
2024-07-24 16:21:56 +0200

    EDM-234: Build RPM for FlightCtl CLI
    Signed-off-by: Ricardo Noriega <rnoriega@redhat.com>

--- hack/build_rpms_packit.sh
+++ hack/build_rpms_packit.sh
@@ -6,7 +6,7 @@ rm $(uname -m)/flightctl-*.rpm 2>/dev/null || true
 rm bin/rpm/* 2>/dev/null || true
 mkdir -p bin/rpm
 # save the spec as packit will modify it locally to inject versioning and we don't want that
-cp packaging/rpm/flightctl-agent.spec /tmp
+cp packaging/rpm/flightctl.spec /tmp
 packit build locally
-cp /tmp/flightctl-agent.spec packaging/rpm
+cp /tmp/flightctl.spec packaging/rpm
 mv $(uname -m)/flightctl-*.rpm bin/rpm
