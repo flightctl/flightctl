daf8d3d3ce99054a54636c1374e28d6bc5e886cc
Abhishek koserwal <akoserwa@redhat.com>
2024-07-31 15:23:14 +0530

    MGMT-18186: adding jwt token auth middleware (#323)

--- internal/config/config.go
+++ internal/config/config.go
@@ -51,7 +51,9 @@ type queueConfig struct {
 }
 
 type authConfig struct {
-	K8sApiUrl string `json:"k8sApiUrl,omitempty"`
+	K8sApiUrl        string `json:"k8sApiUrl,omitempty"`
+	JwksUrl          string `json:"jwksUrl,omitempty"`
+	OidcDiscoveryUrl string `json:"oidcDiscoveryUrl,omitempty"`
 }
 
 func ConfigDir() string {
