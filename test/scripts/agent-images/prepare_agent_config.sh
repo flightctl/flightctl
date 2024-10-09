80423f9c507182900fc91994b4d94991b9afe8da
Miguel Angel Ajo Pelayo <majopela@redhat.com>
2024-10-09 12:52:40 +0200

    MGMT-18708: Retry API login and reduce agent intervals

--- test/scripts/agent-images/prepare_agent_config.sh
+++ test/scripts/agent-images/prepare_agent_config.sh
@@ -3,4 +3,10 @@
 mkdir -p bin/agent/etc/flightctl/certs
 
 echo Requesting enrollment enrollment certificate/key and config for agent =====
-./bin/flightctl certificate request -n client-enrollment -d bin/agent/etc/flightctl/certs/ | tee bin/agent/etc/flightctl/config.yaml
+./bin/flightctl certificate request -d bin/agent/etc/flightctl/certs/ | tee bin/agent/etc/flightctl/config.yaml
+
+# enforce the agent to fetch the spec and update status every 2 seconds to improve the E2E test speed
+cat <<EOF | tee -a  bin/agent/etc/flightctl/config.yaml
+spec-fetch-interval: 0m2s
+status-update-interval: 0m2s
+EOF
