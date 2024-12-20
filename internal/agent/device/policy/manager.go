08b496cc5aa4d2894412071e818097a08057b649
Sam Batschelet <sbatsche@redhat.com>
2024-12-20 15:02:41 -0500

    EDM-526: policy: add schedule tests and improve logic
    Signed-off-by: Sam Batschelet <sbatsche@redhat.com>

--- internal/agent/device/policy/manager.go
+++ internal/agent/device/policy/manager.go
@@ -11,18 +11,6 @@ import (
 	"github.com/robfig/cron/v3"
 )
 
-type schedule struct {
-	location           *time.Location
-	cron               cron.Schedule
-	startGraceDuration time.Duration
-}
-
-// Ready returns true if the schedule is ready to run
-func (s *schedule) Ready() bool {
-	now := time.Now().In(s.location)
-	return s.cron.Next(now).Before(now.Add(s.startGraceDuration))
-}
-
 type manager struct {
 	download *schedule
 	update   *schedule
@@ -50,8 +38,8 @@ func (m *manager) Sync(ctx context.Context, desired *v1alpha1.RenderedDeviceSpec
 		return nil
 	}
 	if desired.UpdatePolicy.DownloadSchedule != nil {
-		schedule, err := parseSchedule(desired.UpdatePolicy.DownloadSchedule)
-		if err != nil {
+		schedule := newSchedule(Download)
+		if err := schedule.Parse(desired.UpdatePolicy.DownloadSchedule); err != nil {
 			return fmt.Errorf("failed to parse download schedule: %w", err)
 		}
 		m.download = schedule
@@ -60,8 +48,8 @@ func (m *manager) Sync(ctx context.Context, desired *v1alpha1.RenderedDeviceSpec
 	}
 
 	if desired.UpdatePolicy.UpdateSchedule != nil {
-		schedule, err := parseSchedule(desired.UpdatePolicy.UpdateSchedule)
-		if err != nil {
+		schedule := newSchedule(Update)
+		if err := schedule.Parse(desired.UpdatePolicy.UpdateSchedule); err != nil {
 			return fmt.Errorf("failed to parse update schedule: %w", err)
 		}
 		m.update = schedule
@@ -81,26 +69,43 @@ func (m *manager) IsReady(ctx context.Context, policyType Type) bool {
 		if m.download == nil {
 			return true
 		}
-		return m.download.Ready()
+		return m.download.IsReady(m.log)
 	}
 
 	if policyType == Update {
 		if m.update == nil {
 			return true
 		}
-		return m.update.Ready()
+		return m.update.IsReady(m.log)
 	}
 
 	return false
 }
 
-func parseSchedule(updateSchedule *v1alpha1.UpdateSchedule) (*schedule, error) {
-	s := &schedule{}
+type schedule struct {
+	policyType         Type
+	location           *time.Location
+	cron               cron.Schedule
+	startGraceDuration time.Duration
+	interval           time.Duration
+
+	// this is used for testing to override time.Now
+	nowFn func() time.Time
+}
+
+func newSchedule(policyType Type) *schedule {
+	return &schedule{
+		policyType: policyType,
+		nowFn:      time.Now,
+	}
+}
+
+func (s *schedule) Parse(updateSchedule *v1alpha1.UpdateSchedule) error {
 	// parse time zone
 	if updateSchedule.TimeZone != nil {
 		loc, err := time.LoadLocation(util.FromPtr(updateSchedule.TimeZone))
 		if err != nil {
-			return nil, fmt.Errorf("invalid time zone: %w", err)
+			return fmt.Errorf("invalid time zone: %w", err)
 		}
 		s.location = loc
 	} else {
@@ -111,18 +116,38 @@ func parseSchedule(updateSchedule *v1alpha1.UpdateSchedule) (*schedule, error) {
 	parser := cron.NewParser(cron.Minute | cron.Hour | cron.Dom | cron.Month | cron.Dow)
 	cronExpr, err := parser.Parse(updateSchedule.At)
 	if err != nil {
-		return nil, fmt.Errorf("invalid cron expression: %w", err)
+		return fmt.Errorf("invalid cron expression: %w", err)
 	}
 	s.cron = cronExpr
+
+	// calculate cron interval
+	nextRun := s.cron.Next(s.nowFn().In(s.location))
+	secondRun := s.cron.Next(nextRun)
+	s.interval = secondRun.Sub(nextRun)
 
 	// parse grace duration
 	if updateSchedule.StartGraceDuration != nil {
 		duration, err := time.ParseDuration(*updateSchedule.StartGraceDuration)
 		if err != nil {
-			return nil, fmt.Errorf("invalid start grace duration: %w", err)
+			return fmt.Errorf("invalid start grace duration: %w", err)
 		}
 		s.startGraceDuration = duration
 	}
 
-	return s, nil
+	return nil
+}
+
+// Ready returns true if the schedule is ready to run
+func (s *schedule) IsReady(log *log.PrefixLogger) bool {
+	now := s.nowFn().In(s.location)
+	lastRun := s.cron.Next(now.Add(-s.interval))
+	graceEnd := lastRun.Add(s.startGraceDuration)
+
+	if now.Equal(lastRun) || (now.After(lastRun) && !now.After(graceEnd)) {
+		log.Infof("Policy %s schedule is ready to run. Current time: %s, last run: %s, grace ends: %s", s.policyType, now, lastRun, graceEnd)
+		return true
+	}
+
+	log.Debugf("Policy %s schedule is not ready. Current time: %s, last run: %s, grace ends: %s", s.policyType, now, lastRun, graceEnd)
+	return false
 }
