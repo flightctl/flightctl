82a1399d10e94d35c40ebb8b9cbe8041d4ec353b
Ori Amizur <oamizur@redhat.com>
2025-01-01 15:52:17 +0200

    NO-ISSUE: Fix strategy for rollout device selection
    1. Move strategy to BatchSequence.  This causes the function
       'ValueByDiscriminator' to function prperly.
    
    2. Add utility functions for percentage in api.

--- api/v1alpha1/types.gen.go
+++ api/v1alpha1/types.gen.go
@@ -208,6 +208,7 @@ type Batch_Limit struct {
 type BatchSequence struct {
 	// Sequence A list of batch definitions.
 	Sequence *[]Batch `json:"sequence,omitempty"`
+	Strategy string   `json:"strategy"`
 }
 
 // CPUResourceMonitorSpec Specification for monitoring a resource.
@@ -1266,9 +1267,7 @@ type ResourceSyncStatus struct {
 
 // RolloutDeviceSelection Describes how to select devices for rollout.
 type RolloutDeviceSelection struct {
-	// Strategy The rollout strategy to use.
-	Strategy string `json:"strategy"`
-	union    json.RawMessage
+	union json.RawMessage
 }
 
 // RolloutPolicy RolloutPolicy is the rollout policy of the fleet.
@@ -2322,8 +2321,7 @@ func (t RolloutDeviceSelection) AsBatchSequence() (BatchSequence, error) {
 
 // FromBatchSequence overwrites any union data inside the RolloutDeviceSelection as the provided BatchSequence
 func (t *RolloutDeviceSelection) FromBatchSequence(v BatchSequence) error {
-	t.Strategy = "BatchSequence"
-
+	v.Strategy = "BatchSequence"
 	b, err := json.Marshal(v)
 	t.union = b
 	return err
@@ -2331,8 +2329,7 @@ func (t *RolloutDeviceSelection) FromBatchSequence(v BatchSequence) error {
 
 // MergeBatchSequence performs a merge with any union data inside the RolloutDeviceSelection, using the provided BatchSequence
 func (t *RolloutDeviceSelection) MergeBatchSequence(v BatchSequence) error {
-	t.Strategy = "BatchSequence"
-
+	v.Strategy = "BatchSequence"
 	b, err := json.Marshal(v)
 	if err != nil {
 		return err
@@ -2366,43 +2363,10 @@ func (t RolloutDeviceSelection) ValueByDiscriminator() (interface{}, error) {
 
 func (t RolloutDeviceSelection) MarshalJSON() ([]byte, error) {
 	b, err := t.union.MarshalJSON()
-	if err != nil {
-		return nil, err
-	}
-	object := make(map[string]json.RawMessage)
-	if t.union != nil {
-		err = json.Unmarshal(b, &object)
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	object["strategy"], err = json.Marshal(t.Strategy)
-	if err != nil {
-		return nil, fmt.Errorf("error marshaling 'strategy': %w", err)
-	}
-
-	b, err = json.Marshal(object)
 	return b, err
 }
 
 func (t *RolloutDeviceSelection) UnmarshalJSON(b []byte) error {
 	err := t.union.UnmarshalJSON(b)
-	if err != nil {
-		return err
-	}
-	object := make(map[string]json.RawMessage)
-	err = json.Unmarshal(b, &object)
-	if err != nil {
-		return err
-	}
-
-	if raw, found := object["strategy"]; found {
-		err = json.Unmarshal(raw, &t.Strategy)
-		if err != nil {
-			return fmt.Errorf("error reading 'strategy': %w", err)
-		}
-	}
-
 	return err
 }
