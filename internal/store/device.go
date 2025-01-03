c254c0b803e93cbc4b7d4edbff2d8307c916b850
Lily Sturmann <lkatalin@users.noreply.github.com>
2025-01-03 12:52:58 -0500

    EDM-390: Add decommission spec to rendered device config
    Signed-off-by: Lily Sturmann <lkatalin@users.noreply.github.com>

--- internal/store/device.go
+++ internal/store/device.go
@@ -595,6 +595,7 @@ func (s *DeviceStore) GetRendered(ctx context.Context, orgId uuid.UUID, name str
 		Resources:       device.Spec.Data.Resources,
 		Console:         console,
 		Applications:    device.RenderedApplications.Data,
+		Decommission:    device.Spec.Data.DecommissionRequested,
 	}
 
 	return &renderedConfig, nil
