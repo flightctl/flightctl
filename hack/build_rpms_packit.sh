21e5f0f1a89a6f4610a25f9abe8e14b514d58c07
Gregory Shilin <gshilin@redhat.com>
2025-01-12 09:59:13 +0200

    EDM-885: RPM versioning

--- hack/build_rpms_packit.sh
+++ hack/build_rpms_packit.sh
@@ -2,12 +2,11 @@
 set -ex
 # this only works on rpm based systems, for non-rpm this is wrapped by build_rpms.sh
 packit 2>/dev/null >/dev/null || (echo "Installing packit" && sudo dnf install -y packit)
-rm "$(uname -m)"/flightctl-*.rpm 2>/dev/null || true
-rm bin/rpm/* 2>/dev/null || true
+rm -f "$(uname -m)"/flightctl-*.rpm 2>/dev/null || true
+rm -f bin/rpm/* 2>/dev/null || true
 mkdir -p bin/rpm
 # save the spec as packit will modify it locally to inject versioning and we don't want that
 cp packaging/rpm/flightctl.spec /tmp
-git describe --tags --exclude latest
 packit build locally
 cp /tmp/flightctl.spec packaging/rpm
 mv $(uname -m)/flightctl-*.rpm bin/rpm
