9f150bb3ca21cc73e8fd11aa69f80d4845b053a6
Sam Batschelet <sbatsche@redhat.com>
2025-02-07 10:08:04 -0500

    NO-ISSUE: test/agent: improve container workflow
    Signed-off-by: Sam Batschelet <sbatsche@redhat.com>

--- deploy/agent-vm.mk
+++ deploy/agent-vm.mk
@@ -35,10 +35,13 @@ clean-agent-vm:
 agent-container: BUILD_TYPE := regular
 agent-container: bin/output/qcow2/disk.qcow2
 	@echo "Starting Agent Container flightctl-agent from $(CONTAINER_NAME)"
-	podman run -d --name flightctl-agent localhost:5000/"$(CONTAINER_NAME)"
+	sudo podman run -d --network host --name flightctl-agent localhost:5000/$(CONTAINER_NAME)
+
+run-agent-container:
+	sudo podman run -d --network host -v ./bin/flightctl-agent:/usr/bin/flightctl-agent:Z --name flightctl-agent localhost:5000/$(CONTAINER_NAME)
 
 clean-agent-container:
-	podman stop flightctl-agent
-	podman rm flightctl-agent
+	sudo podman stop flightctl-agent || true
+	sudo podman rm flightctl-agent || true
 
-.PHONY: agent-container
+.PHONY: agent-container run-agent-container clean-agent-container
