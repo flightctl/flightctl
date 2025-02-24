974aa232f611679aa62fb6a084c3c965a6d70797
Samuel de la Cruz <sdelacru@redhat.com>
2025-02-24 14:22:28 +0100

    EDM-799: added tests for decommission via CLI

---
+++ test/e2e/decommission/decommission.go
@@ -0,0 +1,59 @@
+package decommission_test
+
+import (
+	"testing"
+
+	"github.com/flightctl/flightctl/api/v1alpha1"
+	"github.com/flightctl/flightctl/test/harness/e2e"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"github.com/sirupsen/logrus"
+)
+
+const TIMEOUT = "1m"
+const POLLING = "250ms"
+
+func TestDecommission(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Decommission E2E Suite")
+}
+
+var _ = Describe("CLI decommission test", func() {
+	var (
+		harness  *e2e.Harness
+		deviceId string
+	)
+
+	BeforeEach(func() {
+		harness = e2e.NewTestHarness()
+		deviceId = harness.StartVMAndEnroll()
+	})
+
+	AfterEach(func() {
+		harness.Cleanup(false)
+	})
+
+	Context("decommission", func() {
+		It("should decommission a device via CLI", Label("decommission", "rh-799"), func() {
+			out, err := harness.CLI("decommission", "devices/"+deviceId)
+			Expect(err).NotTo(HaveOccurred())
+			logrus.Info(out)
+			Expect(out).To(ContainSubstring("Device scheduled for decommissioning: 200 OK:"))
+			harness.WaitForDeviceContents(deviceId, "The device has completed decommissioning and will wipe its management certificate",
+				func(device *v1alpha1.Device) bool {
+					return conditionExists(device, "DeviceDecommissioning", "True", string(v1alpha1.DecommissionStateComplete))
+				}, "10m")
+		})
+	})
+})
+
+func conditionExists(device *v1alpha1.Device, conditionType, conditionStatus, conditionReason string) bool {
+	for _, condition := range device.Status.Conditions {
+		if string(condition.Type) == conditionType &&
+			condition.Reason == conditionReason &&
+			string(condition.Status) == conditionStatus {
+			return true
+		}
+	}
+	return false
+}
