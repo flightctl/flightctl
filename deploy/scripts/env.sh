1d4aed84a0bafb5462ea1dce66244c1e43a96bf1
Dakota Crowder <dcrowder@redhat.com>
2025-03-05 13:47:00 -0500

    EDM-1186: Add secrets generation and usage to quadlets deployment

--- deploy/scripts/env.sh
+++ deploy/scripts/env.sh
@@ -11,3 +11,35 @@ start_service() {
     echo "Starting $service_name"
     systemctl --user start "$service_name"
 }
+
+generate_secret() {
+    echo $(cat /dev/urandom | tr -dc 'A-Za-z0-9' | fold -w5 | head -n4 | paste -sd'-')
+}
+
+create_secrets() {
+    if [ -z "${FLIGHTCTL_POSTGRESQL_PASSWORD}" ]; then
+        FLIGHTCTL_POSTGRESQL_PASSWORD=$(generate_secret)
+    fi
+    if [ -z "${FLIGHTCTL_POSTGRESQL_MASTER_PASSWORD}" ]; then
+        FLIGHTCTL_POSTGRESQL_MASTER_PASSWORD=$(generate_secret)
+    fi
+    if [ -z "${FLIGHTCTL_POSTGRESQL_USER_PASSWORD}" ]; then
+        FLIGHTCTL_POSTGRESQL_USER_PASSWORD=$(generate_secret)
+    fi
+    if [ -z "${FLIGHTCTL_KV_PASSWORD}" ]; then
+        FLIGHTCTL_KV_PASSWORD=$(generate_secret)
+    fi
+
+    if ! podman secret inspect flightctl-postgresql-password &>/dev/null; then
+        echo -n "${FLIGHTCTL_POSTGRESQL_PASSWORD}" | podman secret create flightctl-postgresql-password -
+    fi
+    if ! podman secret inspect flightctl-postgresql-master-password &>/dev/null; then
+        echo -n "${FLIGHTCTL_POSTGRESQL_MASTER_PASSWORD}" | podman secret create flightctl-postgresql-master-password -
+    fi
+    if ! podman secret inspect flightctl-postgresql-user-password &>/dev/null; then
+        echo -n "${FLIGHTCTL_POSTGRESQL_USER_PASSWORD}" | podman secret create flightctl-postgresql-user-password -
+    fi
+    if ! podman secret inspect flightctl-kv-password &>/dev/null; then
+        echo -n "${FLIGHTCTL_KV_PASSWORD}" | podman secret create flightctl-kv-password -
+    fi
+}
