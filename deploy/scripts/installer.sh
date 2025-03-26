345757dcdc0d9e0f0de8eb13352f87b549b4fd88
Dakota Crowder <dcrowder@redhat.com>
2025-03-25 21:40:38 -0400

    EDM-1186: Template/move files for all services

--- deploy/scripts/installer.sh
+++ deploy/scripts/installer.sh
@@ -16,6 +16,14 @@ SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 # TODO rename env,sh to utils or something else
 source "${SCRIPT_DIR}"/env.sh
 
+export CONFIG_OUTPUT_DIR
+# Conditionally set FLIGHTCTL_DISABLE_AUTH if AUTH_TYPE="none"
+if [[ "$AUTH_TYPE" == "none" ]]; then
+    export FLIGHTCTL_DISABLE_AUTH="true"
+else
+    unset FLIGHTCTL_DISABLE_AUTH
+fi
+
 validate_inputs() {
     if [ -z "$BASE_DOMAIN" ]; then
         echo "Error: BASE_DOMAIN is not set"
@@ -32,34 +40,43 @@ validate_inputs() {
     fi
 }
 
-template_api() {
-    local api_template="${TEMPLATE_DIR}/flightctl-api/flightctl-api.container"
-    local api_config="${TEMPLATE_DIR}/flightctl-api/flightctl-api-config/config.yaml.template"
+render_service() {
+    local service_name="$1"
 
-    local output_template="${QUADLET_FILES_OUTPUT_DIR}/flightctl-api.container"
-    local output_config="${CONFIG_OUTPUT_DIR}/flightctl-api/config.yaml"
+    # Process container template
+    envsubst < "${TEMPLATE_DIR}/flightctl-${service_name}/flightctl-${service_name}.container" > "${QUADLET_FILES_OUTPUT_DIR}/flightctl-${service_name}.container"
 
-    export CONFIG_OUTPUT_DIR
-    # Conditionally set FLIGHTCTL_DISABLE_AUTH if AUTH_TYPE="none"
-    if [[ "$AUTH_TYPE" == "none" ]]; then
-        export FLIGHTCTL_DISABLE_AUTH="true"
-    else
-        unset FLIGHTCTL_DISABLE_AUTH
-    fi
+    # Ensure config output directory exists
+    mkdir -p "${CONFIG_OUTPUT_DIR}/flightctl-${service_name}"
 
-    # Ensure output directories exist
-    mkdir -p "$(dirname "$output_template")"
-    mkdir -p "$(dirname "$output_config")"
+    # Process all files in the config directory
+    for config_file in "${TEMPLATE_DIR}/flightctl-${service_name}/flightctl-${service_name}-config"/*; do
+        if [[ -f "$config_file" ]]; then
+            envsubst < "$config_file" > "${CONFIG_OUTPUT_DIR}/flightctl-${service_name}/$(basename "$config_file")"
+        fi
+    done
 
-    # Ensure output files exist or create empty ones before overwriting
-    : > "$output_template"
-    : > "$output_config"
+    # Move any .volume file if it exists
+    for volume in "${TEMPLATE_DIR}/flightctl-${service_name}"/*.volume; do
+        if [[ -f "$volume" ]]; then
+            cp "$volume" "${QUADLET_FILES_OUTPUT_DIR}"
+        fi
+    done
+}
 
-    # Process the container template
-    envsubst '${CONFIG_OUTPUT_DIR} ${FLIGHTCTL_DISABLE_AUTH}' < "$api_template" > "$output_template"
+render_files() {
+    # Copy the network and slice files
+    mkdir -p "${QUADLET_FILES_OUTPUT_DIR}"
+    cp "${TEMPLATE_DIR}/flightctl.network" "${QUADLET_FILES_OUTPUT_DIR}"
+    cp "${TEMPLATE_DIR}/flightctl.slice" "${QUADLET_FILES_OUTPUT_DIR}"
 
-    # Process the config template
-    envsubst '${BASE_DOMAIN} ${FLIGHTCTL_DISABLE_AUTH}' < "$api_config" > "$output_config"
+    render_service "api"
+    render_service "periodic"
+    render_service "worker"
+    render_service "db"
+    render_service "kv"
+    render_service "ui"
+
 }
 
 # Execution
@@ -68,5 +85,4 @@ set -e
 
 validate_inputs
 ensure_secrets
-template_api
-
+render_files
