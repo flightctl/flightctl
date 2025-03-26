abeb41f91b043f5600ead3b3070be5f4bec8efbc
Dakota Crowder <dcrowder@redhat.com>
2025-03-26 13:56:08 -0400

    EDM-1186: Get all services running, modify db to mount startup script

---
+++ deploy/test-config/flightctl-db/enable-superuser.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+_psql () { psql --set ON_ERROR_STOP=1 "$@" ; }
+
+# Ensure POSTGRESQL_MASTER_USER is treated as a superuser
+if [ -n "${POSTGRESQL_MASTER_USER}" ]; then
+    echo "Granting superuser privileges to ${POSTGRESQL_MASTER_USER}"
+    _psql -c "ALTER ROLE ${POSTGRESQL_MASTER_USER} WITH SUPERUSER;"
+fi
