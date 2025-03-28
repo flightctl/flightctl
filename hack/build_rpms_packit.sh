336826bce998209eb16538c80d22b294c18182fb
Dakota Crowder <dcrowder@redhat.com>
2025-03-28 09:27:32 -0400

    EDM-1186: Split out rpm spec into different dir

--- hack/build_rpms_packit.sh
+++ hack/build_rpms_packit.sh
@@ -6,10 +6,10 @@ rm -f "$(uname -m)"/flightctl-*.rpm 2>/dev/null || true
 rm -f bin/rpm/* 2>/dev/null || true
 mkdir -p bin/rpm
 # save the specs as packit will modify it locally to inject versioning and we don't want that
-cp packaging/rpm/flightctl-quadlet-installer.spec /tmp
 cp packaging/rpm/flightctl.spec /tmp
+cp packaging/rpm-installer/flightctl-quadlet-installer.spec /tmp
 packit build locally
 cp /tmp/flightctl.spec packaging/rpm
-cp /tmp/flightctl-quadlet-installer.spec packaging/rpm
+cp /tmp/flightctl-quadlet-installer.spec packaging/rpm-installer
 mv noarch/flightctl-*.rpm bin/rpm
 mv $(uname -m)/flightctl-*.rpm bin/rpm
