255bb897a54594fb9bd30dea1df55d7e06ec6bd7
Dakota Crowder <dcrowder@redhat.com>
2025-03-28 10:51:49 -0400

    EDM-1186: Escape env var in psql init script

--- deploy/podman/flightctl-db/flightctl-db-config/enable-superuser.sh
+++ deploy/podman/flightctl-db/flightctl-db-config/enable-superuser.sh
@@ -5,5 +5,5 @@ _psql () { psql --set ON_ERROR_STOP=1 "$@" ; }
 # Ensure POSTGRESQL_MASTER_USER is treated as a superuser
 if [ -n "${POSTGRESQL_MASTER_USER}" ]; then
     echo "Granting superuser privileges to ${POSTGRESQL_MASTER_USER}"
-    _psql -c "ALTER ROLE ${POSTGRESQL_MASTER_USER} WITH SUPERUSER;"
+    _psql -c "ALTER ROLE $(echo "SELECT quote_ident('${POSTGRESQL_MASTER_USER}');" | _psql -t -A) WITH SUPERUSER;"
 fi
