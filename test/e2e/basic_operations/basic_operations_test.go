7b819c89ef86d088a2395fa0e08d860fd856fffa
Amir Yogev <ayogev@redhat.com>
2025-05-13 14:34:00 +0300

    Add end-to-end tests for basic operations, label selectors and field selectors. These tests enhance and add test cases not covered by test/e2e/selectors/selectors_test.go which eventually should be removed.
    Signed-off-by: Amir Yogev <ayogev@redhat.com>

---
+++ test/e2e/basic_operations/basic_operations_test.go
@@ -0,0 +1,75 @@
+package basic_operations
+
+import (
+	"fmt"
+	"regexp"
+	"strings"
+	"testing"
+
+	"github.com/flightctl/flightctl/test/e2e/resources"
+	"github.com/flightctl/flightctl/test/harness/e2e"
+	"github.com/flightctl/flightctl/test/util"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+func TestBasicOperations(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Basic Operations E2E Suite")
+}
+
+var _ = Describe("Basic Operations", func() {
+	const createdResource = "201 Created"
+
+	var (
+		harness *e2e.Harness
+	)
+
+	AfterEach(func() {
+		Expect(harness.CleanUpAllResources()).To(Succeed())
+	})
+
+	DescribeTable("Create a resource from example file",
+		func(resourceType string, fileName string, extractResourceNameFromExampleFile func(*e2e.Harness, string) (string, error)) {
+			name, err := extractResourceNameFromExampleFile(harness, fileName)
+			Expect(err).ShouldNot(HaveOccurred())
+			Expect(name).ShouldNot(BeEmpty(), fmt.Sprintf("Resource name should not be empty for %s", fileName))
+
+			Expect(resources.ExpectNotExistWithName(harness, resourceType, name)).To(Succeed())
+
+			output, err := resources.ApplyFromExampleFile(harness, fileName)
+			Expect(err).ShouldNot(HaveOccurred())
+
+			matched, err := regexp.MatchString(createdResource, output)
+			Expect(err).ShouldNot(HaveOccurred())
+			Expect(matched).To(BeTrue(), fmt.Sprintf("Expected output to match pattern '%s'", createdResource))
+		},
+		Entry("Create a device from example file", "device", "device.yaml", extractDeviceNameFromExampleFile),
+		Entry("Create a fleet from example file", "fleet", "fleet.yaml", extractFleetNameFromExampleFile),
+		Entry("Create a repository from example file", "repository", "repository-flightctl.yaml", extractRepositoryNameFromExampleFile),
+	)
+})
+
+func extractDeviceNameFromExampleFile(harness *e2e.Harness, deviceFileName string) (string, error) {
+	device := harness.GetDeviceByYaml(util.GetTestExamplesYamlPath(deviceFileName))
+	if &device == nil || &device.Metadata == nil || device.Metadata.Name == nil {
+		return "", fmt.Errorf("device name should not be empty")
+	}
+	return strings.TrimSpace(*device.Metadata.Name), nil
+}
+
+func extractFleetNameFromExampleFile(harness *e2e.Harness, fleetFileName string) (string, error) {
+	fleet := harness.GetFleetByYaml(util.GetTestExamplesYamlPath(fleetFileName))
+	if &fleet == nil || &fleet.Metadata == nil || fleet.Metadata.Name == nil {
+		return "", fmt.Errorf("fleet name should not be empty")
+	}
+	return strings.TrimSpace(*fleet.Metadata.Name), nil
+}
+
+func extractRepositoryNameFromExampleFile(harness *e2e.Harness, repositoryFileName string) (string, error) {
+	repository := harness.GetRepositoryByYaml(util.GetTestExamplesYamlPath(repositoryFileName))
+	if &repository == nil || &repository.Metadata == nil || repository.Metadata.Name == nil {
+		return "", fmt.Errorf("repository name should not be empty")
+	}
+	return strings.TrimSpace(*repository.Metadata.Name), nil
+}
