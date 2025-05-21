ecaadf32db98526b6ce709a40c1302744ab3b6c7
Samuel de la Cruz <sdelacru@redhat.com>
2025-05-21 10:28:39 +0200

    draft: cypress UI testing

---
+++ test/e2e/cypress/README.md
@@ -0,0 +1,15 @@
+# Cypress testing 
+## Installation
+
+For installing the cypress testing tool is needed to have:
+    - node.js installed
+    - npm or yarn (npm preferred)
+    - flightctl installed in openshift deployment (no local testing, only on cluster)
+
+install with:
+    npm install cypress --save-dev
+
+## Execution
+
+To run the app in cypress folder you need to run:
+    npx cypress open
---
+++ test/e2e/cypress/e2e/fleet.cy.js
@@ -0,0 +1,32 @@
+describe('template spec', () => {
+    it('passes', () => {
+        cy.visit('https://console-openshift-console.apps.ocp-edge-cluster-0.qe.lab.redhat.com')
+        cy.wait(9000)
+        cy.origin('https://oauth-openshift.apps.ocp-edge-cluster-0.qe.lab.redhat.com', () => {
+
+            cy.contains('kube:admin').click()
+            cy.get('#inputUsername').should('exist')
+            cy.get('#inputUsername').should('be.visible')
+            cy.get('#inputPassword').should('exist')
+            cy.get('#inputPassword').should('be.visible')
+            cy.get('#inputUsername').type('kubeadmin')
+            cy.get('#inputPassword').type('Dtfw2-z9s49-YPfam-etIFK')
+            cy.contains('button', 'Log in').click()
+        })
+        cy.wait(20000)
+        cy.get('.pf-v5-c-modal-box__close > .pf-v5-c-button').should('be.visible')
+        cy.get('.pf-v5-c-modal-box__close > .pf-v5-c-button').click()
+        cy.url().should('include', 'https://console-openshift-console.apps.ocp-edge-cluster-0.qe.lab.redhat.com')
+        cy.get('#nav-toggle').click()
+        cy.contains('Edge Management').click()
+        cy.contains('Fleets').click()
+        cy.get(':nth-child(2) > .pf-v5-l-split > :nth-child(1) > .pf-v5-c-button').should('exist')
+        cy.get(':nth-child(2) > .pf-v5-l-split > :nth-child(1) > .pf-v5-c-button').should('be.visible')
+        cy.get(':nth-child(2) > .pf-v5-l-split > :nth-child(1) > .pf-v5-c-button').click()
+        cy.get('#rich-validation-field-name').should('be.visible')
+        cy.get('#rich-validation-field-name').type('test-fleet')
+        cy.get('#rich-validation-field-name').should('have.value', 'test-fleet')
+        cy.get('.pf-v5-l-stack__item > .pf-v5-c-label-group > .pf-v5-c-label-group__main > .pf-v5-c-label-group__list').click()
+
+    })
+})
