d78207edec5d0e4b56fd0fd5e6c5b34d0b4fc500
Celia Amador <camadorg@redhat.com>
2025-10-03 09:47:55 +0200

    EDM-2240: Default to YAML format when using --rendered

--- internal/cli/get.go
+++ internal/cli/get.go
@@ -113,7 +113,7 @@ func (o *GetOptions) Bind(fs *pflag.FlagSet) {
 	fs.Int32Var(&o.Limit, FlagLimit, o.Limit, "The maximum number of results returned in the list response. If the value is 0, then the result is not limited.")
 	fs.StringVar(&o.Continue, FlagContinue, o.Continue, "Query more results starting from the value of the 'continue' field in the previous response.")
 	fs.StringVar(&o.FleetName, FlagFleetName, o.FleetName, "Fleet name for accessing templateversions (use only when getting templateversions).")
-	fs.BoolVar(&o.Rendered, FlagRendered, false, "Return the rendered device configuration that is presented to the device.")
+	fs.BoolVar(&o.Rendered, FlagRendered, false, "Return the rendered device configuration that is presented to the device. Default output format is YAML.")
 	fs.BoolVarP(&o.Summary, FlagSummary, "s", false, "Display summary information.")
 	fs.BoolVar(&o.SummaryOnly, FlagSummaryOnly, false, "Display summary information only.")
 	fs.BoolVar(&o.LastSeen, FlagLastSeen, false, "Display the last seen timestamp of the device.")
@@ -181,14 +181,7 @@ func (o *GetOptions) extractResourceArgsFromCmdLine(args []string) []string {
 }
 
 func (o *GetOptions) shouldShowHelpFlag(rule FlagContextualRule, resourceKind, operation string) bool {
-	resourceMatches := false
-	for _, rt := range rule.ResourceKind {
-		if rt == resourceKind {
-			resourceMatches = true
-			break
-		}
-	}
-	if !resourceMatches {
+	if !slices.Contains(rule.ResourceKind, resourceKind) {
 		return false
 	}
 	for _, op := range rule.Operations {
@@ -203,6 +196,12 @@ func (o *GetOptions) Complete(cmd *cobra.Command, args []string) error {
 	if err := o.GlobalOptions.Complete(cmd, args); err != nil {
 		return err
 	}
+
+	// --rendered flag defaults to YAML output when no explicit output format is specified
+	if o.Rendered && o.Output == "" {
+		o.Output = string(display.YAMLFormat)
+	}
+
 	return nil
 }
 
