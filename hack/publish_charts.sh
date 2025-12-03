99debf7b20f2923300b34852c1efcbbf55a51e04
Ben Keith <bkeith@redhat.com>
2025-12-03 09:55:06 -0500

    NO-ISSUE: Fix version discovery
    Make the RPM spec use the version variable, which is based on the
    hack/current-version script in packit config
    
    Make the helm chart publisher script use the hack/current-version script
    as well.

--- hack/publish_charts.sh
+++ hack/publish_charts.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
 set -e
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+set -x
 QUAY_CHARTS=${QUAY_CHARTS:-quay.io/flightctl/charts}
-FLIGHTCTL_VERSION=$(git describe --long --tags)
+FLIGHTCTL_VERSION=$(${SCRIPT_DIR}/current-version)
 FLIGHTCTL_VERSION=${FLIGHTCTL_VERSION#v} # remove the leading v prefix for version
 
 VERSION=${VERSION:-$FLIGHTCTL_VERSION}
