ec682f336671677e5ee03c39ab5ddb3fc8521ca7
Frank A. Zdarsky <fzdarsky@redhat.com>
2025-12-20 13:41:44 +0100

    EDM-2836: Separate UI and CLI artifacts certs

--- deploy/podman/flightctl-cli-artifacts/flightctl-cli-artifacts-config/init.sh
+++ deploy/podman/flightctl-cli-artifacts/flightctl-cli-artifacts-config/init.sh
@@ -21,27 +21,27 @@ sed '/^\s*listen\s*\[::\]:8090/d' "$NGINX_CONFIG_OUTPUT" > "${NGINX_CONFIG_OUTPU
 sed '/^\s*listen\s*8090 ssl/d' "$NGINX_CONFIG_OUTPUT" > "${NGINX_CONFIG_OUTPUT}.ipv6"
 
 # Wait for certificates
-wait_for_files "$CERTS_SOURCE_PATH/flightctl-api/server.crt" "$CERTS_SOURCE_PATH/flightctl-api/server.key"
+wait_for_files "$CERTS_SOURCE_PATH/flightctl-cli-artifacts/server.crt" "$CERTS_SOURCE_PATH/flightctl-cli-artifacts/server.key"
 
 # Handle server certificates
 #
 # The CLI artifacts container runs as user 1001 by default,
 # so we need to ensure that the server certificate and key files
 # can be read by this user.
-if [ -f "$CERTS_SOURCE_PATH/flightctl-api/server.crt" ]; then
-  cp "$CERTS_SOURCE_PATH/flightctl-api/server.crt" "$CERTS_DEST_PATH/server.crt"
+if [ -f "$CERTS_SOURCE_PATH/flightctl-cli-artifacts/server.crt" ]; then
+  cp "$CERTS_SOURCE_PATH/flightctl-cli-artifacts/server.crt" "$CERTS_DEST_PATH/server.crt"
   chown 1001:0 "$CERTS_DEST_PATH/server.crt"
   chmod 0440 "$CERTS_DEST_PATH/server.crt"
 else
-  echo "Error: Server certificate not found at $CERTS_SOURCE_PATH/flightctl-api/server.crt"
+  echo "Error: Server certificate not found at $CERTS_SOURCE_PATH/flightctl-cli-artifacts/server.crt"
   exit 1
 fi
-if [ -f "$CERTS_SOURCE_PATH/flightctl-api/server.key" ]; then
-  cp "$CERTS_SOURCE_PATH/flightctl-api/server.key" "$CERTS_DEST_PATH/server.key"
+if [ -f "$CERTS_SOURCE_PATH/flightctl-cli-artifacts/server.key" ]; then
+  cp "$CERTS_SOURCE_PATH/flightctl-cli-artifacts/server.key" "$CERTS_DEST_PATH/server.key"
   chown 1001:0 "$CERTS_DEST_PATH/server.key"
   chmod 0440 "$CERTS_DEST_PATH/server.key"
 else
-  echo "Error: Server key not found at $CERTS_SOURCE_PATH/flightctl-api/server.key"
+  echo "Error: Server key not found at $CERTS_SOURCE_PATH/flightctl-cli-artifacts/server.key"
   exit 1
 fi
 
