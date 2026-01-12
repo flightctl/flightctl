ee9a87c4d79b44f4efb4de49f053d2992ec27cdb
Ilya Skornyakov <iskornya@redhat.com>
2026-01-12 16:59:59 +0200

    EDM-2980: fix merge conflict

--- internal/agent/device/image_pruning/manager.go
+++ internal/agent/device/image_pruning/manager.go
@@ -8,7 +8,7 @@ import (
 	"sync/atomic"
 	"time"
 
-	"github.com/flightctl/flightctl/api/v1beta1"
+	"github.com/flightctl/flightctl/api/core/v1beta1"
 	"github.com/flightctl/flightctl/internal/agent/client"
 	"github.com/flightctl/flightctl/internal/agent/config"
 	"github.com/flightctl/flightctl/internal/agent/device/applications/lifecycle"
