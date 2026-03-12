6e535d5229279ef729857cd7e83cb330e557eb87
amalykhi <amalykhi@redhat.com>
2026-03-12 12:12:05 +0100

    EDM-3502: add setup fail marker to SetupWorkerHarnessWithoutVM()

--- test/e2e/applications/rootless/rootless_suite_test.go
+++ test/e2e/applications/rootless/rootless_suite_test.go
@@ -19,8 +19,7 @@ func TestRootless(t *testing.T) {
 }
 
 var _ = BeforeSuite(func() {
-	_, _, err := e2e.SetupWorkerHarness()
-	Expect(err).ToNot(HaveOccurred())
+	e2e.SetupWorkerHarnessOrAbort()
 })
 
 var _ = BeforeEach(func() {
