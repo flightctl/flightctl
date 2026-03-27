75113d8f195ad1cf80aad8e858693000de694024
Frank A. Zdarsky <fzdarsky@redhat.com>
2026-03-27 12:54:33 +0100

    NO-ISSUE: Automatic refresh of base images
    Signed-off-by: Frank A. Zdarsky <fzdarsky@redhat.com>

--- hack/build_flightctl-base.sh
+++ hack/build_flightctl-base.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-IMAGE_REPO=${IMAGE_REPO:-quay.io/flightctl/flightctl-base}
-IMAGE_TAG=9.7-1762965531
+RELEASE_VERSION=${RELEASE_VERSION:-9}
+IMAGE_TAG=${IMAGE_TAG:-9.7-1762965531}
+IMAGE_REPO=${IMAGE_REPO:-quay.io/flightctl/flightctl-base-el${RELEASE_VERSION}}
 
 arch=$(uname -m)
 case $arch in
@@ -10,17 +11,18 @@ case $arch in
     aarch64) arch=arm64;;
 esac
 
-container=$(buildah from registry.redhat.io/ubi9-micro:$IMAGE_TAG)
+container=$(buildah from "registry.access.redhat.com/ubi${RELEASE_VERSION}/ubi-micro:${IMAGE_TAG}")
 
-mountdir=$(buildah mount $container)
+mountdir=$(buildah mount "$container")
 dnf install \
-    --installroot $mountdir \
-    --releasever 9 \
+    --installroot "$mountdir" \
+    --releasever "${RELEASE_VERSION}" \
     --setopt install_weak_deps=false \
     --nodocs -y \
     openssl-libs tzdata
 dnf clean all \
-    --installroot $mountdir
-buildah umount $container
+    --installroot "$mountdir"
+buildah umount "$container"
 
-buildah commit $container $IMAGE_REPO:$arch-$IMAGE_TAG
+buildah commit "$container" "${IMAGE_REPO}:${arch}-${IMAGE_TAG}"
+buildah rm "$container"
