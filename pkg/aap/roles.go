df41dee18de8cf8fe131ec534568326bf7d8d928
Asaf Ben Natan <asafbennatan@gmail.com>
2026-05-12 13:13:05 +0300

    EDM-3887: taking AAP team role assignment into account
    Signed-off-by: Asaf Ben Natan <asafbennatan@gmail.com>

--- pkg/aap/roles.go
+++ pkg/aap/roles.go
@@ -48,6 +48,29 @@ type AAPRoleUserAssignment struct {
 
 type AAPRoleUserAssignmentsResponse = AAPPaginatedResponse[AAPRoleUserAssignment]
 
+// AAPTeamSummary represents a team in role assignment summary fields
+type AAPTeamSummary struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+// AAPRoleTeamAssignmentSummaryFields contains nested summary information for team role assignments
+type AAPRoleTeamAssignmentSummaryFields struct {
+	RoleDefinition AAPRoleDefinition `json:"role_definition"`
+	Team           AAPTeamSummary    `json:"team"`
+	ContentObject  AAPContentObject  `json:"content_object"`
+}
+
+// AAPRoleTeamAssignment represents a team's role assignment to an organization
+type AAPRoleTeamAssignment struct {
+	ID             int                                `json:"id"`
+	SummaryFields  AAPRoleTeamAssignmentSummaryFields `json:"summary_fields"`
+	ContentType    string                             `json:"content_type"`
+	ObjectID       string                             `json:"object_id"`
+	RoleDefinition int                                `json:"role_definition"`
+	Team           int                                `json:"team"`
+}
+
 // GET /api/controller/v2/role_user_assignments/?user__id={user_id}
 func (a *AAPGatewayClient) ListUserRoleAssignments(ctx context.Context, token string, userID string) ([]*AAPRoleUserAssignment, error) {
 	// Build query parameters using url.Values
@@ -59,4 +82,16 @@ func (a *AAPGatewayClient) ListUserRoleAssignments(ctx context.Context, token st
 
 	endpoint := a.buildEndpoint("/api/controller/v2/role_user_assignments/", query)
 	return getWithPagination[AAPRoleUserAssignment](a, ctx, endpoint, token)
+}
+
+// GET /api/controller/v2/role_team_assignments/?team__id={team_id}
+func (a *AAPGatewayClient) ListTeamRoleAssignments(ctx context.Context, token string, teamID string) ([]*AAPRoleTeamAssignment, error) {
+	query := url.Values{}
+	query.Set("team__id", teamID)
+	if a.maxPageSize != nil {
+		query.Set("page_size", fmt.Sprintf("%d", *a.maxPageSize))
+	}
+
+	endpoint := a.buildEndpoint("/api/controller/v2/role_team_assignments/", query)
+	return getWithPagination[AAPRoleTeamAssignment](a, ctx, endpoint, token)
 }
